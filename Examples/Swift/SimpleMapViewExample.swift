e24f65423ecf6b00bfcbdae9aa83225a16540280
Jason Wray <jason@kulturny.com>
2016-01-29 14:08:10 -0500

    Unhardwrap line

--- Examples/Swift/SimpleMapViewExample.swift
+++ Examples/Swift/SimpleMapViewExample.swift
@@ -18,9 +18,7 @@ class SimpleMapViewExample_Swift: UIViewController {
         mapView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
 
         // set the map's center coordinate
-        mapView.setCenterCoordinate(CLLocationCoordinate2D(latitude: 40.7326808,
-            longitude: -73.9843407),
-            zoomLevel: 12, animated: false)
+        mapView.setCenterCoordinate(CLLocationCoordinate2D(latitude: 40.7326808, longitude: -73.9843407), zoomLevel: 12, animated: false)
         view.addSubview(mapView)
     }
 }
