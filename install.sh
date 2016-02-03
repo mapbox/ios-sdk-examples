ac1409e411155c37734158837e55342a22cf48a0
Jason Wray <jason@kulturny.com>
2016-02-03 18:24:35 -0500

    Bump to v3.1.0-pre.3

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-MBGLVERSION='3.1.0-pre.2'
+MBGLVERSION='3.1.0-pre.3'
 MBGLURL="https://github.com/mapbox/mapbox-gl-native/releases/download/ios-v$MBGLVERSION/mapbox-ios-sdk-$MBGLVERSION-symbols-dynamic.zip"
 MBGLVANITYNAME="Mapbox iOS SDK v$MBGLVERSION"
 TEMPZIPNAME="framework-$MBGLVERSION.zip"
