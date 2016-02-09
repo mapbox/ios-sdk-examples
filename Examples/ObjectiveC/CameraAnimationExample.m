77bbdd7c2e00282842e49a1ca240697043903870
Jason Wray <jason@kulturny.com>
2016-02-08 19:17:39 -0800

    Delay camera set until viewDidAppear
    Work around https://github.com/mapbox/mapbox-gl-native/issues/3859

--- Examples/ObjectiveC/CameraAnimationExample.m
+++ Examples/ObjectiveC/CameraAnimationExample.m
@@ -11,27 +11,37 @@
 
 NSString *const MBXExampleCameraAnimation = @"CameraAnimationExample";
 
+@interface CameraAnimationExample ()
+
+@property (nonatomic) MGLMapView *mapView;
+
+@end
+
 @implementation CameraAnimationExample
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    MGLMapView *mapView = [[MGLMapView alloc] initWithFrame:self.view.bounds];
-    mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    self.mapView = [[MGLMapView alloc] initWithFrame:self.view.bounds];
+    self.mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
 
     CLLocationCoordinate2D center = CLLocationCoordinate2DMake(50.999, 3.3253);
 
     // Optionally set a starting point, rotated 180°.
-    [mapView setCenterCoordinate:center zoomLevel:5 direction:180 animated:NO];
+    [self.mapView setCenterCoordinate:center zoomLevel:5 direction:180 animated:NO];
 
-    [self.view addSubview:mapView];
+    [self.view addSubview:self.mapView];
+}
+
+-(void)viewDidAppear:(BOOL)animated {
+    // Wait a bit before setting a new camera.
 
     // Create a camera that rotates around the same center point, back to 0°.
     // `fromDistance:` is meters above mean sea level that an eye would have to be in order to see what the map view is showing.
-    MGLMapCamera *camera = [MGLMapCamera cameraLookingAtCenterCoordinate:center fromDistance:9000 pitch:45 heading:0];
+    MGLMapCamera *camera = [MGLMapCamera cameraLookingAtCenterCoordinate:self.mapView.centerCoordinate fromDistance:9000 pitch:45 heading:0];
 
     // Animate the camera movement over 5 seconds.
-    [mapView setCamera:camera withDuration:5 animationTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut]];
+    [self.mapView setCamera:camera withDuration:5 animationTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut]];
 }
 
 @end
--- Examples/Swift/CameraAnimationExample.swift
+++ Examples/Swift/CameraAnimationExample.swift
@@ -12,21 +12,28 @@ import Mapbox
 
 class CameraAnimationExample_Swift: UIViewController {
 
+    var mapView: MGLMapView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let mapView = MGLMapView(frame: view.bounds)
+        mapView = MGLMapView(frame: view.bounds)
         mapView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
 
         let center = CLLocationCoordinate2D(latitude: 50.999, longitude: 3.3253)
 
         // Optionally set a starting point, rotated 180°.
         mapView.setCenterCoordinate(center, zoomLevel: 5, direction: 180, animated: false)
+
         view.addSubview(mapView)
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        // Wait a bit before setting a new camera.
 
         // Create a camera that rotates around the same center point, back to 0°.
         // `fromDistance:` is meters above mean sea level that an eye would have to be in order to see what the map view is showing.
-        let camera = MGLMapCamera(lookingAtCenterCoordinate: center, fromDistance: 9000, pitch: 45, heading: 0)
+        let camera = MGLMapCamera(lookingAtCenterCoordinate: mapView.centerCoordinate, fromDistance: 9000, pitch: 45, heading: 0)
 
         // Animate the camera movement over 5 seconds.
         mapView.setCamera(camera, withDuration: 5, animationTimingFunction: CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut))
