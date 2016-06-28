18c3679c2f572b122ea9248a4e3d0feda23184c8
Jason Wray <jason@kulturny.com>
2016-06-27 23:49:42 -0400

    Use polyline coordinates as point annotation coordinates in model example
    Users often ask how to add more than one unique image annotation, so here they go.

--- Examples/Swift/CustomAnnotationModelExample.swift
+++ Examples/Swift/CustomAnnotationModelExample.swift
@@ -22,14 +22,6 @@ class CustomAnnotationModelExample_Swift: UIViewController, MGLMapViewDelegate {
         mapView.delegate = self
         view.addSubview(mapView)
 
-        // Point Annotation
-        let point = CustomPointAnnotation(coordinate: CLLocationCoordinate2DMake(0, 0),
-                                          title: "Custom Point Annotation",
-                                          subtitle: nil)
-        // Set the custom `image` and `reuseIdentifier` properties, later used in the `mapView:imageForAnnotation:` delegate method.
-        point.reuseIdentifier = "yeOldeGreyDot"
-        point.image = dotWithSize(20)
-
         // Polyline
         // Create a coordinates array with all of the coordinates for our polyline.
         var coordinates = [
@@ -45,12 +37,36 @@ class CustomAnnotationModelExample_Swift: UIViewController, MGLMapViewDelegate {
         // Set the custom `color` property, later used in the `mapView:strokeColorForShapeAnnotation:` delegate method.
         polyline.color = .darkGrayColor()
 
-        // Add both annotations to the map.
-        mapView.addAnnotations([point, polyline])
+        // Add the polyline to the map. Note that this method name is singular.
+        mapView.addAnnotation(polyline)
+
+        // Point Annotations
+        // Add a custom point annotation for every coordinate (vertex) in the polyline.
+        var pointAnnotations = [CustomPointAnnotation]()
+        for coordinate in coordinates {
+            let count = pointAnnotations.count + 1
+            let point = CustomPointAnnotation(coordinate: coordinate,
+                                              title: "Custom Point Annotation \(count)",
+                                              subtitle: nil)
+
+            // Set the custom `image` and `reuseIdentifier` properties, later used in the `mapView:imageForAnnotation:` delegate method.
+            // Create a unique reuse identifier for each new annotation image.
+            point.reuseIdentifier = "customAnnotation\(count)"
+            // This dot image grows in size as more annotations are added to the array.
+            point.image = dot(size:5 * count)
+
+            // Append each annotation to the array, which will be added to the map all at once.
+            pointAnnotations.append(point)
+        }
+
+        // Add the point annotations to the map. This time the method name is plural.
+        // If you have multiple annotations to add, batching their addition to the map is more efficient.
+        mapView.addAnnotations(pointAnnotations)
     }
 
-    func dotWithSize(size: CGFloat) -> (UIImage) {
-        let rect = CGRectMake(0, 0, size, size)
+    func dot(size size: Int) -> (UIImage) {
+        let floatSize = (CGFloat)(size)
+        let rect = CGRectMake(0, 0, floatSize, floatSize)
         let strokeWidth: CGFloat = 1
 
         UIGraphicsBeginImageContextWithOptions(rect.size, false, UIScreen.mainScreen().scale)
