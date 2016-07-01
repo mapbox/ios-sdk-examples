f1e43ed896f8bbec371dedb61b8ec8cd46e39619
Jason Wray <jason@kulturny.com>
2016-07-01 17:51:44 -0400

    Add tracking mode button to Tracking Modes example (Swift)

--- Examples/Swift/UserTrackingModesExample.swift
+++ Examples/Swift/UserTrackingModesExample.swift
@@ -13,34 +13,144 @@ import Mapbox
 class UserTrackingModesExample_Swift: UIViewController, MGLMapViewDelegate {
 
     var mapView: MGLMapView!
+    @IBOutlet var button: UserLocationButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        mapView = MGLMapView(frame: view.bounds, styleURL: MGLStyle.lightStyleURLWithVersion(9))
+        mapView = MGLMapView(frame: view.bounds, styleURL: MGLStyle.darkStyleURLWithVersion(9))
         mapView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        mapView.delegate = self
+
+        mapView.tintColor = .redColor()
+        mapView.attributionButton.tintColor = .lightGrayColor()
 
         view.addSubview(mapView)
 
-        view.addSubview(locateButton())
+        setupLocationButton()
     }
 
-    override func viewDidAppear(animated: Bool) {
-        //mapView.setTargetCoordinate(CLLocationCoordinate2D(latitude: 45, longitude: 122), animated: true)
+    func mapView(mapView: MGLMapView, didChangeUserTrackingMode mode: MGLUserTrackingMode, animated: Bool) {
+        button.updateArrow(for: mode)
     }
 
-    func locateButton() -> UIButton {
-        let button = UIButton()
-        button.frame = CGRectMake(0, 0, 60, 60)
-        button.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(1)
+    @IBAction func locationButtonTapped() {
+        var mode: MGLUserTrackingMode
 
-        button.addTarget(self, action: Selector("locateUser"), forControlEvents: .TouchUpInside)
+        switch (mapView.userTrackingMode) {
+        case .None:
+            mode = .Follow
+            break
+        case .Follow:
+            mode = .FollowWithHeading
+            break
+        case .FollowWithHeading:
+            mode = .FollowWithCourse
+            break
+        case .FollowWithCourse:
+            mode = .None
+            break
+        }
 
-        return button
+        mapView.userTrackingMode = mode
     }
 
-    func locateUser() {
-        mapView.userTrackingMode = .FollowWithCourse
+    func setupLocationButton() {
+        button = UserLocationButton()
+        button.addTarget(self, action: Selector("locationButtonTapped"), forControlEvents: .TouchUpInside)
+        button.tintColor = mapView.tintColor
+        view.addSubview(button)
+
+        // Do some basic auto layout.
+        button.translatesAutoresizingMaskIntoConstraints = false
+
+        let constraints = [
+            NSLayoutConstraint(item: button, attribute: .Top, relatedBy: .GreaterThanOrEqual, toItem: topLayoutGuide, attribute: .Bottom, multiplier: 1, constant: 10),
+            NSLayoutConstraint(item: button, attribute: .Leading, relatedBy: .Equal, toItem: view, attribute: .Leading, multiplier: 1, constant: 10),
+            NSLayoutConstraint(item: button, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: button.frame.size.height),
+            NSLayoutConstraint(item: button, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: button.frame.size.width)
+        ]
+
+        view.addConstraints(constraints)
     }
 
+}
+
+class UserLocationButton : UIButton {
+    private let size: CGFloat = 80
+    private var arrow: CAShapeLayer?
+
+    required init() {
+        super.init(frame: CGRectMake(0, 0, size, size))
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.5)
+        layer.cornerRadius = 4
+
+        layoutArrow()
+    }
+
+    private func layoutArrow() {
+        if arrow == nil {
+            let arrow = CAShapeLayer()
+            arrow.path = arrowPath()
+            arrow.bounds = CGRectMake(0, 0, size / 2, size / 2)
+            arrow.position = CGPointMake(size / 2, size / 2)
+            arrow.shouldRasterize = true
+            arrow.rasterizationScale = UIScreen.mainScreen().scale
+            arrow.drawsAsynchronously = true
+
+            self.arrow = arrow
+            updateArrow(for: .None)
+            layer.addSublayer(self.arrow!)
+        }
+    }
+
+    private func arrowPath() -> CGPath {
+        let max: CGFloat = size / 2
+
+        let bezierPath = UIBezierPath()
+        bezierPath.moveToPoint(CGPointMake(max * 0.5, 0))
+        bezierPath.addLineToPoint(CGPointMake(max * 0.1, max))
+        bezierPath.addLineToPoint(CGPointMake(max * 0.5, max * 0.65))
+        bezierPath.addLineToPoint(CGPointMake(max * 0.9, max))
+        bezierPath.addLineToPoint(CGPointMake(max * 0.5, 0))
+        bezierPath.closePath()
+
+        return bezierPath.CGPath
+    }
+
+    func updateArrow(for mode: MGLUserTrackingMode) {
+        switch (mode) {
+        case .None:
+            arrow!.fillColor = UIColor.clearColor().CGColor
+            arrow!.strokeColor = UIColor.whiteColor().CGColor
+            arrow!.setAffineTransform(CGAffineTransformRotate(CGAffineTransformIdentity, 0))
+            break
+        case .Follow:
+            arrow!.fillColor = UIColor.clearColor().CGColor
+            arrow!.strokeColor = tintColor.CGColor
+            arrow!.setAffineTransform(CGAffineTransformRotate(CGAffineTransformIdentity, 0.66))
+            break
+        case .FollowWithHeading:
+            arrow!.fillColor = tintColor.CGColor
+            arrow!.strokeColor = UIColor.clearColor().CGColor
+            arrow!.setAffineTransform(CGAffineTransformRotate(CGAffineTransformIdentity, 0.66))
+            break
+        case .FollowWithCourse:
+            arrow!.fillColor = tintColor.CGColor
+            arrow!.strokeColor = UIColor.clearColor().CGColor
+            arrow!.setAffineTransform(CGAffineTransformRotate(CGAffineTransformIdentity, 0))
+            break
+        }
+
+        arrow!.position = (mode == .None || mode == .FollowWithCourse) ? CGPointMake(size / 2, size / 2) : CGPointMake(size / 2 + 2, size / 2 - 2)
+
+        layoutIfNeeded()
+    }
 }
