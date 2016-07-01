29136f2d9d38710ca33fa52f6d57749e1b5f667c
Jason Wray <jason@kulturny.com>
2016-06-30 22:32:44 -0400

    Add initial view annotation dragging example (Swift only)

--- Examples/Swift/AnnotationViewExample.swift
+++ Examples/Swift/AnnotationViewExample.swift
@@ -16,37 +16,120 @@ class AnnotationViewExample_Swift: UIViewController, MGLMapViewDelegate {
 
         let mapView = MGLMapView(frame: view.bounds)
         mapView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
-        mapView.styleURL = MGLStyle.satelliteStreetsStyleURLWithVersion(9)
-        mapView.tintColor = .whiteColor()
+        mapView.styleURL = MGLStyle.lightStyleURLWithVersion(9)
+        mapView.tintColor = .darkGrayColor()
         mapView.zoomLevel = 1
         mapView.delegate = self
         view.addSubview(mapView)
 
-        // Point Annotation
-        let point = CustomPointAnnotation(coordinate: CLLocationCoordinate2DMake(0, 0),
-                                          title: "Custom Point Annotation",
-                                          subtitle: nil)
-        // Set the custom `image` and `reuseIdentifier` properties, later used in the `mapView:imageForAnnotation:` delegate method.
-        point.reuseIdentifier = "yeOldeGreyDot"
-        //point.image = dotWithSize(20)
+        let coordinates = [
+            CLLocationCoordinate2DMake(0, -70),
+            CLLocationCoordinate2DMake(0, -35),
+            CLLocationCoordinate2DMake(0,  0),
+            CLLocationCoordinate2DMake(0, 35),
+            CLLocationCoordinate2DMake(0, 70),
+        ]
 
-        // Polyline
-        // Create a coordinates array with all of the coordinates for our polyline.
-        var coordinates = [
-            CLLocationCoordinate2DMake(35, -25),
-            CLLocationCoordinate2DMake(20, -30),
-            CLLocationCoordinate2DMake( 0, -25),
-            CLLocationCoordinate2DMake(-15,  0),
-            CLLocationCoordinate2DMake(-45, 10),
-            CLLocationCoordinate2DMake(-45, 40),
-            ]
+        var pointAnnotations = [MGLPointAnnotation]()
+        for coordinate in coordinates {
+            let point = MGLPointAnnotation()
+            point.coordinate = coordinate
+            point.title = "To drag this annotation, first tap and hold."
+            pointAnnotations.append(point)
+        }
 
-        let polyline = CustomPolyline(coordinates: &coordinates, count: UInt(coordinates.count))
-        // Set the custom `color` property, later used in the `mapView:strokeColorForShapeAnnotation:` delegate method.
-        polyline.color = .darkGrayColor()
-
-        // Add both annotations to the map.
-        mapView.addAnnotations([point, polyline])
+        mapView.addAnnotations(pointAnnotations)
     }
 
+    func mapView(mapView: MGLMapView, viewForAnnotation annotation: MGLAnnotation) -> MGLAnnotationView? {
+        guard annotation is MGLPointAnnotation else {
+            return nil
+        }
+
+        if let annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier("draggablePoint") {
+            return annotationView
+        } else {
+            return CustomDraggableAnnotationView(reuseIdentifier: "draggablePoint", size: 50)
+        }
+    }
+
+    func mapView(mapView: MGLMapView, didDragAnnotationView annotationView: MGLAnnotationView, toCoordinate coordinate: CLLocationCoordinate2D) {
+        if let annotation = annotationView.annotation as? MGLPointAnnotation {
+            // Leave this annotation at the final drag position.
+            // If you do not manually update the coordinate, the annotation will return to its original position.
+            annotation.coordinate = coordinate
+        }
+    }
+
+    func mapView(mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        return true
+    }
+}
+
+// MGLAnnotationView subclass
+class CustomDraggableAnnotationView : MGLAnnotationView {
+    init(reuseIdentifier: String, size: CGFloat) {
+        super.init(reuseIdentifier: reuseIdentifier)
+
+        // `draggable` is a property of MGLAnnotationView, disabled by default.
+        draggable = true
+
+        // This property prevents the annotation from changing size when the map is tilted.
+        scalesWithViewingDistance = false
+
+        // Begin setting up the view.
+        frame = CGRectMake(0, 0, size, size)
+
+        backgroundColor = .darkGrayColor()
+
+        // Use CALayer's corner radius to turn this view into a circle.
+        layer.cornerRadius = size / 2
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.whiteColor().CGColor
+        layer.shadowColor = UIColor.blackColor().CGColor
+        layer.shadowOpacity = 0.1
+    }
+
+    // These two initializers are forced upon us by Swift.
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    //
+    override func setDragState(dragState: MGLAnnotationViewDragState, animated: Bool) {
+        super.setDragState(dragState, animated: animated)
+
+        switch dragState {
+        case .Starting:
+            print("Starting", terminator: "")
+            startDragging()
+            break
+        case .Dragging:
+            print(".", terminator: "")
+        case .Ending, .Canceling:
+            print("Ending")
+            endDragging()
+            break
+        case .None:
+            return
+        }
+    }
+
+    func startDragging() {
+        UIView.animateWithDuration(0.3, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: [], animations: {
+            self.layer.opacity = 0.8
+            self.transform = CGAffineTransformScale(CGAffineTransformIdentity, 1.5, 1.5)
+        }, completion: nil)
+    }
+
+    func endDragging() {
+        UIView.animateWithDuration(0.3, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: [], animations: {
+            self.layer.opacity = 1
+            self.transform = CGAffineTransformScale(CGAffineTransformIdentity, 1, 1)
+        }, completion: nil)
+    }
 }
