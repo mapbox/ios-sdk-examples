c7a10ccab34a9e26bd922711cf84f232842961b8
Jason Wray <jason@kulturny.com>
2016-07-14 16:18:32 -0400

    Update callout delegate example to use fixed coordinate
    `-showAnnotations:animated:` is broken for a single point: https://github.com/mapbox/mapbox-gl-native/pull/5693.

--- Examples/Swift/CalloutDelegateUsageExample.swift
+++ Examples/Swift/CalloutDelegateUsageExample.swift
@@ -23,7 +23,7 @@ class CalloutDelegateUsageExample_Swift: UIViewController,
 
         view.addSubview(mapView)
 
-        // remember to set the delegate (or much of this will not work)
+        // Remember to set the delegate.
         mapView.delegate = self
 
         addAnnotation()
@@ -37,10 +37,10 @@ class CalloutDelegateUsageExample_Swift: UIViewController,
 
         mapView.addAnnotation(annotation)
 
-        // fit the map to the annotation(s)
-        mapView.showAnnotations(mapView.annotations!, animated: false)
+        // Center the map on the annotation.
+        mapView.setCenterCoordinate(annotation.coordinate, zoomLevel: 17, animated: false)
 
-        // pop-up the callout view
+        // Pop-up the callout view.
         mapView.selectAnnotation(annotation, animated: true)
     }
 
@@ -50,6 +50,7 @@ class CalloutDelegateUsageExample_Swift: UIViewController,
 
     func mapView(mapView: MGLMapView, leftCalloutAccessoryViewForAnnotation annotation: MGLAnnotation) -> UIView? {
         if (annotation.title! == "Kinkaku-ji") {
+            // Callout height is fixed; width expands to fit its content.
             let label = UILabel(frame: CGRectMake(0, 0, 60, 50))
             label.textAlignment = .Right
             label.textColor = UIColor(red: 0.81, green: 0.71, blue: 0.23, alpha: 1)
@@ -66,7 +67,7 @@ class CalloutDelegateUsageExample_Swift: UIViewController,
     }
 
     func mapView(mapView: MGLMapView, annotation: MGLAnnotation, calloutAccessoryControlTapped control: UIControl) {
-        // hide the callout view
+        // Hide the callout view.
         mapView.deselectAnnotation(annotation, animated: false)
 
         UIAlertView(title: annotation.title!!, message: "A lovely (if touristy) place.", delegate: nil, cancelButtonTitle: nil, otherButtonTitles: "OK").show()
