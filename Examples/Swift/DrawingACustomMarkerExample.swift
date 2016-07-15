468d020dde27f4195c887f17043b71c8d382683e
Jason Wray <jason@kulturny.com>
2016-07-15 00:26:40 -0400

    Update marker image examples

--- Examples/Swift/DrawingACustomMarkerExample.swift
+++ Examples/Swift/DrawingACustomMarkerExample.swift
@@ -11,40 +11,36 @@ import Mapbox
 @objc(DrawingACustomMarkerExample_Swift)
 
 class DrawingACustomMarkerExample_Swift: UIViewController, MGLMapViewDelegate {
-
-    var mapView: MGLMapView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        mapView = MGLMapView(frame: view.bounds, styleURL: MGLStyle.lightStyleURLWithVersion(9))
+        let mapView = MGLMapView(frame: view.bounds, styleURL: MGLStyle.lightStyleURLWithVersion(9))
         mapView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
         mapView.tintColor = .darkGrayColor()
         
-        // Set the map's bounds to Pisa, Italy
+        // Set the map's bounds to Pisa, Italy.
         let bounds = MGLCoordinateBounds(sw: CLLocationCoordinate2D(latitude: 43.7115, longitude: 10.3725),
             ne: CLLocationCoordinate2D(latitude: 43.7318, longitude: 10.4222))
         mapView.setVisibleCoordinateBounds(bounds, animated: false)
         
         view.addSubview(mapView)
 
-        // Set the map view‘s delegate property
+        // Set the map view‘s delegate property.
         mapView.delegate = self
         
-        // Initialize and add the marker annotation
+        // Initialize and add the point annotation.
         let pisa = MGLPointAnnotation()
         pisa.coordinate = CLLocationCoordinate2DMake(43.72305, 10.396633)
         pisa.title = "Leaning Tower of Pisa"
-
         mapView.addAnnotation(pisa)
     }
 
     func mapView(mapView: MGLMapView, imageForAnnotation annotation: MGLAnnotation) -> MGLAnnotationImage? {
-        // Try to reuse the existing ‘pisa’ annotation image, if it exists
+        // Try to reuse the existing ‘pisa’ annotation image, if it exists.
         var annotationImage = mapView.dequeueReusableAnnotationImageWithIdentifier("pisa")
         
         if annotationImage == nil {
-            // Leaning Tower of Pisa by Stefan Spieler from the Noun Project
+            // Leaning Tower of Pisa by Stefan Spieler from the Noun Project.
             var image = UIImage(named: "pisavector")!
 
             // The anchor point of an annotation is currently always the center. To
@@ -56,7 +52,7 @@ class DrawingACustomMarkerExample_Swift: UIViewController, MGLMapViewDelegate {
             // with a custom alignment rect that excludes the padding.
             image = image.imageWithAlignmentRectInsets(UIEdgeInsetsMake(0, 0, image.size.height/2, 0))
 
-            // Initialize the ‘pisa’ annotation image with the UIImage we just loaded
+            // Initialize the ‘pisa’ annotation image with the UIImage we just loaded.
             annotationImage = MGLAnnotationImage(image: image, reuseIdentifier: "pisa")
         }
         
@@ -64,7 +60,7 @@ class DrawingACustomMarkerExample_Swift: UIViewController, MGLMapViewDelegate {
     }
 
     func mapView(mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
-        // Always allow callouts to popup when annotations are tapped
+        // Always allow callouts to popup when annotations are tapped.
         return true
     }
 }
