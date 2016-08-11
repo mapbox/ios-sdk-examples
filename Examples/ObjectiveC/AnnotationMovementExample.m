d0a4f5f3ea7510b39b635540cfa9e043f3253149
Jason Wray <jason@kulturny.com>
2016-08-11 04:59:30 -0400

    Blank most of the ObjC example

--- Examples/ObjectiveC/AnnotationMovementExample.m
+++ Examples/ObjectiveC/AnnotationMovementExample.m
@@ -39,46 +39,6 @@ NSString *const MBXExampleAnnotationMovement = @"AnnotationMovementExample";
     return self;
 }
 
-- (void)setDragState:(MGLAnnotationViewDragState)dragState animated:(BOOL)animated {
-    [super setDragState:dragState animated:animated];
-
-    switch (dragState) {
-        case MGLAnnotationViewDragStateStarting:
-            printf("Starting");
-            [self startDragging];
-            break;
-
-        case MGLAnnotationViewDragStateDragging:
-            printf(".");
-            break;
-
-        case MGLAnnotationViewDragStateEnding:
-        case MGLAnnotationViewDragStateCanceling:
-            printf("Ending\n");
-            [self endDragging];
-            break;
-
-        case MGLAnnotationViewDragStateNone:
-            return;
-    }
-}
-
-// When the user interacts with an annotation, animate opacity and scale changes.
-- (void)startDragging {
-    [UIView animateWithDuration:0.3 delay:0 usingSpringWithDamping:0.5 initialSpringVelocity:0 options:0 animations:^{
-        self.layer.opacity = 0.8;
-        self.transform = CGAffineTransformScale(CGAffineTransformIdentity, 1.5, 1.5);
-    } completion:nil];
-}
-
-- (void)endDragging {
-    self.transform = CGAffineTransformScale(CGAffineTransformIdentity, 1.5, 1.5);
-    [UIView animateWithDuration:0.3 delay:0 usingSpringWithDamping:0.5 initialSpringVelocity:0 options:0 animations:^{
-        self.layer.opacity = 1;
-        self.transform = CGAffineTransformScale(CGAffineTransformIdentity, 1, 1);
-    } completion:nil];
-}
-
 @end
 
 //
@@ -98,55 +58,7 @@ NSString *const MBXExampleAnnotationMovement = @"AnnotationMovementExample";
     mapView.zoomLevel = 1;
     mapView.delegate = self;
     [self.view addSubview:mapView];
-
-    // Specify coordinates for our annotations.
-    CLLocationCoordinate2D coordinates[] = {
-        CLLocationCoordinate2DMake(0, -70),
-        CLLocationCoordinate2DMake(0, -35),
-        CLLocationCoordinate2DMake(0,  0),
-        CLLocationCoordinate2DMake(0, 35),
-        CLLocationCoordinate2DMake(0, 70),
-    };
-    NSUInteger numberOfCoordinates = sizeof(coordinates) / sizeof(CLLocationCoordinate2D);
-
-    // Fill an array with point annotations and add it to the map.
-    NSMutableArray *pointAnnotations = [NSMutableArray arrayWithCapacity:numberOfCoordinates];
-    for (NSUInteger i = 0; i < numberOfCoordinates; i++) {
-        CLLocationCoordinate2D coordinate = coordinates[i];
-        MGLPointAnnotation *point = [[MGLPointAnnotation alloc] init];
-        point.coordinate = coordinate;
-        point.title = @"To drag this annotation, first tap and hold.";
-        [pointAnnotations addObject:point];
-    }
-
-    [mapView addAnnotations:pointAnnotations];
 }
 
-#pragma mark - MGLMapViewDelegate methods
-
-// This delegate method is where you tell the map to load a view for a specific annotation. To load a static MGLAnnotationImage, you would use `-mapView:imageForAnnotation:`.
-- (MGLAnnotationView *)mapView:(MGLMapView *)mapView viewForAnnotation:(id <MGLAnnotation>)annotation {
-    // This example is only concerned with point annotations.
-    if (![annotation isKindOfClass:[MGLPointAnnotation class]]) {
-        return nil;
-    }
-
-    // Use the point annotation’s longitude value (as a string) as the reuse identifier for its view.
-    NSString *reuseIdentifier = [NSString stringWithFormat:@"%f", annotation.coordinate.longitude];
-
-    // For better performance, always try to reuse existing annotations. To use multiple different annotation views, change the reuse identifier for each.
-    MoveableAnnotationView *annotationView = [mapView dequeueReusableAnnotationViewWithIdentifier:@"draggablePoint"];
-
-    // If there’s no reusable annotation view available, initialize a new one.
-    if (!annotationView) {
-        annotationView = [[MoveableAnnotationView alloc] initWithReuseIdentifier:reuseIdentifier size:50];
-    }
-
-    return annotationView;
-}
-
-- (BOOL)mapView:(MGLMapView *)mapView annotationCanShowCallout:(id<MGLAnnotation>)annotation {
-    return YES;
-}
 
 @end
