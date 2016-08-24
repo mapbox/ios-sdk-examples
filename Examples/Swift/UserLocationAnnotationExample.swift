8a50120d80914364fd279e88caefe60bdf1d96a7
Jason Wray <jason@kulturny.com>
2016-08-24 12:18:43 -0400

    Add comments; simplify layer implementation

--- Examples/Swift/UserLocationAnnotationExample.swift
+++ Examples/Swift/UserLocationAnnotationExample.swift
@@ -10,6 +10,7 @@ import Mapbox
 
 @objc(UserLocationAnnotationExample_Swift)
 
+// Example view controller
 class UserLocationAnnotationExample_Swift: UIViewController, MGLMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,12 +19,16 @@ class UserLocationAnnotationExample_Swift: UIViewController, MGLMapViewDelegate 
         mapView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
         mapView.delegate = self
 
+        // Enable heading tracking mode so that the arrow will appear.
         mapView.userTrackingMode = .FollowWithHeading
 
         view.addSubview(mapView)
     }
 
+    // MARK: - MGLMapViewDelegate methods
+
     func mapView(mapView: MGLMapView, viewForAnnotation annotation: MGLAnnotation) -> MGLAnnotationView? {
+        // This is how we substitute a custom view for the user location annotation.
         if annotation is MGLUserLocation && mapView.userLocation != nil {
             return CustomUserLocationAnnotationView()
         }
@@ -32,6 +37,8 @@ class UserLocationAnnotationExample_Swift: UIViewController, MGLMapViewDelegate 
     }
 }
 
+//
+// MGLUserLocationAnnotationView subclass
 class CustomUserLocationAnnotationView: MGLUserLocationAnnotationView {
     let size: CGFloat = 25
     var arrowSize: CGFloat!
@@ -44,6 +51,8 @@ class CustomUserLocationAnnotationView: MGLUserLocationAnnotationView {
             frame = CGRectMake(0, 0, size, size)
         }
 
+        // This method can be called many times a second, so be careful to keep it lightweight.
+
         if CLLocationCoordinate2DIsValid(self.userLocation!.coordinate) {
             setupLayers()
             updateHeading()
@@ -51,9 +60,11 @@ class CustomUserLocationAnnotationView: MGLUserLocationAnnotationView {
     }
 
     private func updateHeading() {
+        // Show the heading arrow, if we’re able to.
         if let heading = userLocation!.heading where mapView?.userTrackingMode == .FollowWithHeading {
             arrow.hidden = false
 
+            // Rotate the arrow according to the user’s heading.
             let rotation = CGAffineTransformRotate(
                 CGAffineTransformIdentity,
                 -MGLRadiansFromDegrees(mapView!.direction - heading.trueHeading))
@@ -73,10 +84,8 @@ class CustomUserLocationAnnotationView: MGLUserLocationAnnotationView {
             dot = CALayer()
             dot.bounds = CGRectMake(0, 0, size, size)
             dot.position = CGPointMake(super.bounds.size.width / 2, super.bounds.size.height / 2)
-            dot.shouldRasterize = true
-            dot.rasterizationScale = UIScreen.mainScreen().scale
-            dot.drawsAsynchronously = true
 
+            // Use CALayer’s corner radius to turn this layer into a circle.
             dot.cornerRadius = size / 2
             dot.backgroundColor = super.tintColor.CGColor
             dot.borderWidth = 2
@@ -94,11 +103,10 @@ class CustomUserLocationAnnotationView: MGLUserLocationAnnotationView {
 
             arrow = CAShapeLayer()
             arrow.path = arrowPath()
+
+            // We use a long, skinny bounds so that the center remains the same as the dot. This makes rotation easier.
             arrow.bounds = CGRectMake(0, 0, arrowSize, size + (arrowSize * 2))
             arrow.position = CGPointMake(super.bounds.size.width / 2, 0)
-            arrow.shouldRasterize = true
-            arrow.rasterizationScale = UIScreen.mainScreen().scale
-            arrow.drawsAsynchronously = true
 
             arrow.fillColor = super.tintColor.CGColor
 
