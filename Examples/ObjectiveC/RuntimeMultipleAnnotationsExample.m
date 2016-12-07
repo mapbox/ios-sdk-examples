3e3bbae5c18da77178e67a5cc84073985977d73c
Eric Wolfe <eric.r.wolfe@gmail.com>
2016-12-06 18:17:29 -0800

    Update multiple annotations example to show how to add custom icons

--- Examples/ObjectiveC/RuntimeMultipleAnnotationsExample.m
+++ Examples/ObjectiveC/RuntimeMultipleAnnotationsExample.m
@@ -25,7 +25,13 @@ NSString *const MBXExampleRuntimeMultipleAnnotations = @"RuntimeMultipleAnnotati
     MGLMapView *mapView = [[MGLMapView alloc] initWithFrame:self.view.bounds];
 
     mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+
+    [mapView setCenterCoordinate:CLLocationCoordinate2DMake(37.090240, -95.712891) zoomLevel:2 animated:NO];
+
     mapView.delegate = self;
+
+    // Add our own gesture recognizer to handle taps on our custom map features
+    [mapView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleMapTap:)]];
 
     [self.view addSubview:mapView];
 
@@ -33,15 +39,127 @@ NSString *const MBXExampleRuntimeMultipleAnnotations = @"RuntimeMultipleAnnotati
 }
 
 - (void)mapView:(MGLMapView *)mapView didFinishLoadingStyle:(MGLStyle *)style {
-    [self loadAnnotations];
+    [self fetchPoints:^(NSArray *features) {
+	[self addItemsToMap:features];
+    }];
 }
 
-- (void)loadAnnotations {
+- (void)addItemsToMap:(NSArray *)features {
+    // You can add custom UIImages to the map style
+    // These can be referenced by an MGLSymbolStyleLayer's iconImage property
+    [[self.mapView style] setImage:[UIImage imageNamed:@"lighthouse"] forName:@"lighthouse"];
+
+    // Add the features to the map as a GeoJSONSource
+    MGLGeoJSONSource *source = [[MGLGeoJSONSource alloc] initWithIdentifier:@"lighthouses" features:features options:nil];
+    [[self.mapView style] addSource:source];
+
+    UIColor *lighthouseColor = [UIColor colorWithRed:0.08 green:0.44 blue:0.96 alpha:1.0];
+
+    // Use MGLCircleStyleLayer to represent the points with simple circles
+    // In this case, we can use style functions to gradually change properties between zoom level 2 and 7: the circle opacity from 50% to 100% and the circle radius from 2px to 3px
+    MGLCircleStyleLayer *circles = [[MGLCircleStyleLayer alloc] initWithIdentifier:@"lighthouse-circles" source:source];
+    circles.circleColor = [MGLStyleValue valueWithRawValue:lighthouseColor];
+    circles.circleOpacity = [MGLStyleValue valueWithStops:@{
+	@2: [MGLStyleValue valueWithRawValue:@0.5],
+	@7: [MGLStyleValue valueWithRawValue:@1.0],
+    }];
+    circles.circleRadius = [MGLStyleValue valueWithStops:@{
+	@2: [MGLStyleValue valueWithRawValue:@2],
+	@7: [MGLStyleValue valueWithRawValue:@3],
+    }];
+
+    // Use MGLSymbolStyleLayer for more complex styling of points including custom icons and text rendering
+    MGLSymbolStyleLayer *symbols = [[MGLSymbolStyleLayer alloc] initWithIdentifier:@"lighthouse-symbols" source:source];
+    symbols.iconImage = [MGLStyleValue valueWithRawValue:@"lighthouse"];
+    symbols.iconSize = [MGLStyleValue valueWithRawValue:@0.5];
+    symbols.iconOpacity = [MGLStyleValue valueWithStops:@{
+	@5.9: [MGLStyleValue valueWithRawValue:@0],
+	@6: [MGLStyleValue valueWithRawValue:@1],
+    }];
+    symbols.iconHaloColor = [MGLStyleValue valueWithRawValue:[[UIColor whiteColor] colorWithAlphaComponent:0.5]];
+    symbols.iconHaloWidth = [MGLStyleValue valueWithRawValue:@1];
+    // {name} references the "name" key in an MGLPointFeature's attributes dictionary
+    symbols.textField = [MGLStyleValue valueWithRawValue:@"{name}"];
+    symbols.textColor = symbols.iconColor;
+    symbols.textSize = [MGLStyleValue valueWithStops:@{
+	@10: [MGLStyleValue valueWithRawValue:@10],
+	@16: [MGLStyleValue valueWithRawValue:@16],
+    }];
+    symbols.textTranslate = [MGLStyleValue valueWithRawValue:[NSValue valueWithCGVector:CGVectorMake(10, 0)]];
+    symbols.textOpacity = symbols.iconOpacity;
+    symbols.textHaloColor = symbols.iconHaloColor;
+    symbols.textHaloWidth = symbols.iconHaloWidth;
+    symbols.textJustify = [MGLStyleValue valueWithRawValue:[NSValue valueWithMGLTextJustify:MGLTextJustifyLeft]];
+    symbols.textAnchor = [MGLStyleValue valueWithRawValue:[NSValue valueWithMGLTextAnchor:MGLTextAnchorLeft]];
+
+    [[self.mapView style] addLayer:circles];
+    [[self.mapView style] addLayer:symbols];
+}
+
+#pragma mark - Feature interaction
+
+- (void)handleMapTap:(UITapGestureRecognizer *)sender {
+    if (sender.state == UIGestureRecognizerStateEnded) {
+	// Limit feature selection to just the following layer identifiers
+	NSArray *layerIdentifiers = @[@"lighthouse-symbols", @"lighthouse-circles"];
+
+	CGPoint point = [sender locationInView:sender.view];
+
+	// Try matching the exact point first
+	for (id f in [self.mapView visibleFeaturesAtPoint:point inStyleLayersWithIdentifiers:[NSSet setWithArray:layerIdentifiers]]) {
+	    if ([f isKindOfClass:[MGLPointFeature class]]) {
+		[self showCallout:f];
+		return;
+	    }
+	}
+
+	// Otherwise, get first features within a rect the size of a touch (44x44)
+	CGRect pointRect = {point, CGSizeZero};
+	CGRect touchRect = CGRectInset(pointRect, -22.0, -22.0);
+	for (id f in [self.mapView visibleFeaturesInRect:touchRect inStyleLayersWithIdentifiers:[NSSet setWithArray:layerIdentifiers]]) {
+	    if ([f isKindOfClass:[MGLPointFeature class]]) {
+		[self showCallout:f];
+		return;
+	    }
+	}
+
+	// If no features were found, deselect the selected annotation, if any
+	[self.mapView deselectAnnotation:[[self.mapView selectedAnnotations] firstObject] animated:YES];
+    }
+}
+
+- (void)showCallout:(MGLPointFeature *)feature {
+    MGLPointFeature *point = [[MGLPointFeature alloc] init];
+    point.title = feature.attributes[@"name"];
+    point.coordinate = feature.coordinate;
+
+    // Selecting an feature that doesn't already exist on the map will add a new annotation view
+    // We'll need to use the map's delegate methods to add an empty annotation view and remove it when we're done selecting it
+    [self.mapView selectAnnotation:point animated:YES];
+}
+
+#pragma mark - MGLMapViewDelegate
+
+- (BOOL)mapView:(MGLMapView *)mapView annotationCanShowCallout:(id <MGLAnnotation>)annotation {
+    return YES;
+}
+
+- (void)mapView:(MGLMapView *)mapView didDeselectAnnotation:(id <MGLAnnotation>)annotation {
+    [mapView removeAnnotation:annotation];
+}
+
+- (MGLAnnotationView *)mapView:(MGLMapView *)mapView viewForAnnotation:(id <MGLAnnotation>)annotation {
+    return [[MGLAnnotationView alloc] init];
+}
+
+#pragma mark - Data fetching and parsing
+
+- (void)fetchPoints:(void (^)(NSArray *))completion {
     // Wikidata query for all lighthouses in the United States: https://query.wikidata.org/#%23added%20before%202016-10%0A%23defaultView%3AMap%0ASELECT%20DISTINCT%20%3Fitem%20%3FitemLabel%20%3Fcoor%20%3Fimage%0AWHERE%0A%7B%0A%09%3Fitem%20wdt%3AP31%20wd%3AQ39715%20.%20%0A%09%3Fitem%20wdt%3AP17%20wd%3AQ30%20.%0A%09%3Fitem%20wdt%3AP625%20%3Fcoor%20.%0A%09OPTIONAL%20%7B%20%3Fitem%20wdt%3AP18%20%3Fimage%20%7D%20%20%0A%09SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22en%22%20%20%7D%20%20%0A%7D%0AORDER%20BY%20%3FitemLabel
     NSString *query = @"SELECT DISTINCT ?item "
 	"?itemLabel ?coor ?image "
 	"WHERE "
-	"{ "
+	    "{ "
 	    "?item wdt:P31 wd:Q39715 . "
 	    "?item wdt:P17 wd:Q30 . "
 	    "?item wdt:P625 ?coor . "
@@ -65,54 +183,9 @@ NSString *const MBXExampleRuntimeMultipleAnnotations = @"RuntimeMultipleAnnotati
 	NSArray *items = json[@"results"][@"bindings"];
 	if (!items) return;
 	dispatch_async(dispatch_get_main_queue(), ^{
-	    [self mapItems:items];
+	    completion([self parseJSONItems:items]);
 	});
     }] resume];
-}
-
-- (void)mapItems:(NSArray *)items {
-    NSArray *features = [self parseJSONItems:items];
-
-    MGLGeoJSONSource *source = [[MGLGeoJSONSource alloc] initWithIdentifier:@"lighthouses" features:features options:nil];
-
-    UIColor *lighthouseColor = [UIColor colorWithRed:0.45 green:0.32 blue:0.23 alpha:1.0];
-
-    MGLCircleStyleLayer *circles = [[MGLCircleStyleLayer alloc] initWithIdentifier:@"lighthouse-circles" source:source];
-    circles.circleColor = [MGLStyleValue valueWithRawValue:lighthouseColor];
-    circles.circleOpacity = [MGLStyleValue valueWithStops:@{
-	@2: [MGLStyleValue valueWithRawValue:@0.5],
-	@7: [MGLStyleValue valueWithRawValue:@1.0],
-    }];
-    circles.circleRadius = [MGLStyleValue valueWithStops:@{
-	@2: [MGLStyleValue valueWithRawValue:@2],
-	@7: [MGLStyleValue valueWithRawValue:@3],
-    }];
-
-    MGLSymbolStyleLayer *symbols = [[MGLSymbolStyleLayer alloc] initWithIdentifier:@"lighthouse-symbols" source:source];
-    symbols.iconImage = [MGLStyleValue valueWithRawValue:@"circle-15"];
-    symbols.iconColor = [MGLStyleValue valueWithRawValue:lighthouseColor];
-    symbols.iconOpacity = [MGLStyleValue valueWithStops:@{
-	@5.9: [MGLStyleValue valueWithRawValue:@0],
-	@6: [MGLStyleValue valueWithRawValue:@1],
-    }];
-    symbols.iconHaloColor = [MGLStyleValue valueWithRawValue:[[UIColor whiteColor] colorWithAlphaComponent:0.5]];
-    symbols.iconHaloWidth = [MGLStyleValue valueWithRawValue:@1];
-    symbols.textColor = symbols.iconColor;
-    symbols.textField = [MGLStyleValue valueWithRawValue:@"{name}"];
-    symbols.textSize = [MGLStyleValue valueWithStops:@{
-	@10: [MGLStyleValue valueWithRawValue:@10],
-	@16: [MGLStyleValue valueWithRawValue:@16],
-    }];
-    symbols.textTranslate = [MGLStyleValue valueWithRawValue:[NSValue valueWithCGVector:CGVectorMake(10, 0)]];
-    symbols.textOpacity = symbols.iconOpacity;
-    symbols.textHaloColor = symbols.iconHaloColor;
-    symbols.textHaloWidth = symbols.iconHaloWidth;
-    symbols.textJustify = [MGLStyleValue valueWithRawValue:[NSValue valueWithMGLTextJustify:MGLTextJustifyLeft]];
-    symbols.textAnchor = [MGLStyleValue valueWithRawValue:[NSValue valueWithMGLTextAnchor:MGLTextAnchorLeft]];
-
-    [[self.mapView style] addSource:source];
-    [[self.mapView style] addLayer:circles];
-    [[self.mapView style] addLayer:symbols];
 }
 
 - (NSArray *)parseJSONItems:(NSArray *)items {
@@ -127,6 +200,8 @@ NSString *const MBXExampleRuntimeMultipleAnnotations = @"RuntimeMultipleAnnotati
 
 	MGLPointFeature *feature = [[MGLPointFeature alloc] init];
 	feature.coordinate = CLLocationCoordinate2DMake([pointComponents[1] doubleValue], [pointComponents[0] doubleValue]);
+	feature.title = title;
+	// A feature's attributes can used by runtime styling for things like text labels
 	feature.attributes = @{
 	    @"name": title,
 	};
--- Examples/Swift 3/RuntimeMultipleAnnotationsExample.swift
+++ Examples/Swift 3/RuntimeMultipleAnnotationsExample.swift
@@ -19,59 +19,38 @@ class RuntimeMultipleAnnotationsExample_Swift: UIViewController, MGLMapViewDeleg
 
 	let mapView = MGLMapView(frame: view.bounds)
 	mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+	mapView.setCenter(CLLocationCoordinate2D(latitude: 37.090240, longitude: -95.712891), zoomLevel: 2, animated: false)
+
 	mapView.delegate = self
 
 	view.addSubview(mapView)
+
+	// Add our own gesture recognizer to handle taps on our custom map features
+	mapView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(RuntimeMultipleAnnotationsExample_Swift.handleMapTap(sender:))))
 
 	self.mapView = mapView
     }
 
     func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
-	self.loadAnnotations()
+	self.fetchPoints(withCompletion: { (features) in
+	    self.addItemsToMap(features: features)
+	})
     }
 
-    func loadAnnotations() {
-	// Wikidata query for all lighthouses in the United States: https://query.wikidata.org/#%23added%20before%202016-10%0A%23defaultView%3AMap%0ASELECT%20DISTINCT%20%3Fitem%20%3FitemLabel%20%3Fcoor%20%3Fimage%0AWHERE%0A%7B%0A%09%3Fitem%20wdt%3AP31%20wd%3AQ39715%20.%20%0A%09%3Fitem%20wdt%3AP17%20wd%3AQ30%20.%0A%09%3Fitem%20wdt%3AP625%20%3Fcoor%20.%0A%09OPTIONAL%20%7B%20%3Fitem%20wdt%3AP18%20%3Fimage%20%7D%20%20%0A%09SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22en%22%20%20%7D%20%20%0A%7D%0AORDER%20BY%20%3FitemLabel
-	let query = "SELECT DISTINCT ?item " +
-	    "?itemLabel ?coor ?image " +
-	    "WHERE " +
-	    "{ " +
-	    "?item wdt:P31 wd:Q39715 . " +
-	    "?item wdt:P17 wd:Q30 . " +
-	    "?item wdt:P625 ?coor . " +
-	    "OPTIONAL { ?item wdt:P18 ?image } . " +
-	    "SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\" } " +
-	    "} " +
-	"ORDER BY ?itemLabel"
+    func addItemsToMap(features: [MGLFeature]) {
+	// You can add custom UIImages to the map style
+	// These can be referenced by an MGLSymbolStyleLayer's iconImage property
+	self.mapView.style().setImage(UIImage(named: "lighthouse")!, forName: "lighthouse")
 
-	var characterSet = NSMutableCharacterSet()
-	characterSet.formUnion(with: CharacterSet.urlQueryAllowed)
-	characterSet.removeCharacters(in: "?")
-	characterSet.removeCharacters(in: "&")
-	characterSet.removeCharacters(in: ":")
-
-	let encodedQuery = query.addingPercentEncoding(withAllowedCharacters: characterSet as CharacterSet)!
-
-	let request = URLRequest(url: URL(string: "https://query.wikidata.org/sparql?query=\(encodedQuery)&format=json")!)
-
-	URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
-	    guard let data = data else { return }
-	    guard let json = try? JSONSerialization.jsonObject(with: data, options:[]) as? [String: AnyObject] else { return }
-	    guard let results = json?["results"] as? [String: AnyObject] else { return }
-	    guard let items = results["bindings"] as? [[String: AnyObject]] else { return }
-	    DispatchQueue.main.async {
-		self.mapItems(items: items)
-	    }
-	}).resume()
-    }
-
-    func mapItems(items: [[String: AnyObject]]) {
-	let features = parseJSONItems(items: items)
-
+	// Add the features to the map as a GeoJSONSource
 	let source = MGLGeoJSONSource(identifier: "lighthouses", features: features, options: nil)
+	self.mapView.style().add(source)
 
-	let lighthouseColor = UIColor(red: 0.45, green: 0.32, blue: 0.23, alpha: 1.0)
+	let lighthouseColor = UIColor(red: 0.08, green: 0.44, blue: 0.96, alpha: 1.0)
 
+	// Use MGLCircleStyleLayer to represent the points with simple circles
+	// In this case, we can use style functions to gradually change properties between zoom level 2 and 7: the circle opacity from 50% to 100% and the circle radius from 2px to 3px
 	let circles = MGLCircleStyleLayer(identifier: "lighthouse-circles", source: source)
 	circles.circleColor = MGLStyleValue(rawValue: lighthouseColor)
 	circles.circleOpacity = MGLStyleValue(stops: [
@@ -83,17 +62,20 @@ class RuntimeMultipleAnnotationsExample_Swift: UIViewController, MGLMapViewDeleg
 	    7: MGLStyleValue(rawValue: 3),
 	    ])
 
+	// Use MGLSymbolStyleLayer for more complex styling of points including custom icons and text rendering
 	let symbols = MGLSymbolStyleLayer(identifier: "lighthouse-symbols", source: source)
-	symbols.iconImage = MGLStyleValue(rawValue: "circle-15")
+	symbols.iconImage = MGLStyleValue(rawValue: "lighthouse")
 	symbols.iconColor = MGLStyleValue(rawValue: lighthouseColor)
+	symbols.iconSize = MGLStyleValue(rawValue: 0.5)
 	symbols.iconOpacity = MGLStyleValue(stops: [
 	    5.9: MGLStyleValue(rawValue: 0),
 	    6: MGLStyleValue(rawValue: 1)
 	    ])
 	symbols.iconHaloColor = MGLStyleValue(rawValue: UIColor.white.withAlphaComponent(0.5))
 	symbols.iconHaloWidth = MGLStyleValue(rawValue: 1)
-	symbols.textColor = symbols.iconColor
+	// {name} references the "name" key in an MGLPointFeature's attributes dictionary
 	symbols.textField = MGLStyleValue(rawValue: "{name}")
+	symbols.textColor = symbols.iconColor
 	symbols.textSize = MGLStyleValue(stops: [
 	    10: MGLStyleValue(rawValue: 10),
 	    16: MGLStyleValue(rawValue: 16)
@@ -110,6 +92,108 @@ class RuntimeMultipleAnnotationsExample_Swift: UIViewController, MGLMapViewDeleg
 	self.mapView.style().add(symbols)
     }
 
+    // MARK: - Feature interaction
+    func handleMapTap(sender: UITapGestureRecognizer) {
+	if sender.state == .ended {
+	    // Limit feature selection to just the following layer identifiers
+	    let layerIdentifiers = ["lighthouse-symbols", "lighthouse-circles"]
+
+	    // Try matching the exact point first
+	    let point = sender.location(in: sender.view!)
+	    for f in mapView.visibleFeatures(at: point, styleLayerIdentifiers: Set(layerIdentifiers)) {
+		if let f = f as? MGLPointFeature {
+		    self.showCallout(feature: f)
+		    return
+		}
+	    }
+
+	    let touchCoordinate = mapView.convert(point, toCoordinateFrom: sender.view!)
+	    let touchLocation = CLLocation(latitude: touchCoordinate.latitude, longitude: touchCoordinate.longitude)
+
+	    // Otherwise, get all features within a rect the size of a touch (44x44)
+	    let touchRect = CGRect(origin: point, size: .zero).insetBy(dx: -22.0, dy: -22.0)
+	    var possibleFeatures = [MGLPointFeature]()
+	    for f in mapView.visibleFeatures(in: touchRect, styleLayerIdentifiers: Set(layerIdentifiers)) {
+		if let f = f as? MGLPointFeature {
+		    possibleFeatures.append(f)
+		}
+	    }
+
+	    // Select the closest feature to the touch center
+	    let closestFeatures = possibleFeatures.sorted(by: { (a, b) -> Bool in
+		return CLLocation(latitude: a.coordinate.latitude, longitude: a.coordinate.longitude).distance(from: touchLocation) < CLLocation(latitude: b.coordinate.latitude, longitude: b.coordinate.longitude).distance(from: touchLocation)
+	    })
+	    if let f = closestFeatures.first {
+		self.showCallout(feature: f)
+		return
+	    }
+
+	    // If no features were found, deselect the selected annotation, if any
+	    self.mapView.deselectAnnotation(self.mapView.selectedAnnotations.first, animated: true)
+	}
+    }
+
+    func showCallout(feature: MGLPointFeature) {
+	let point = MGLPointFeature()
+	point.title = feature.attributes["name"] as? String
+	point.coordinate = feature.coordinate
+
+	// Selecting an feature that doesn't already exist on the map will add a new annotation view
+	// We'll need to use the map's delegate methods to add an empty annotation view and remove it when we're done selecting it
+	mapView.selectAnnotation(point, animated: true)
+    }
+
+    // MARK: - MGLMapViewDelegate
+
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+	return true
+    }
+
+    func mapView(_ mapView: MGLMapView, didDeselect annotation: MGLAnnotation) {
+	mapView.removeAnnotations([annotation])
+    }
+
+    func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? {
+	return MGLAnnotationView()
+    }
+
+    // MARK: - Data fetching and parsing
+
+    func fetchPoints(withCompletion completion: @escaping (([MGLFeature]) -> Void)) {
+	// Wikidata query for all lighthouses in the United States: https://query.wikidata.org/#%23added%20before%202016-10%0A%23defaultView%3AMap%0ASELECT%20DISTINCT%20%3Fitem%20%3FitemLabel%20%3Fcoor%20%3Fimage%0AWHERE%0A%7B%0A%09%3Fitem%20wdt%3AP31%20wd%3AQ39715%20.%20%0A%09%3Fitem%20wdt%3AP17%20wd%3AQ30%20.%0A%09%3Fitem%20wdt%3AP625%20%3Fcoor%20.%0A%09OPTIONAL%20%7B%20%3Fitem%20wdt%3AP18%20%3Fimage%20%7D%20%20%0A%09SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22en%22%20%20%7D%20%20%0A%7D%0AORDER%20BY%20%3FitemLabel
+	let query = "SELECT DISTINCT ?item " +
+	    "?itemLabel ?coor ?image " +
+	    "WHERE " +
+	    "{ " +
+		"?item wdt:P31 wd:Q39715 . " +
+		"?item wdt:P17 wd:Q30 . " +
+		"?item wdt:P625 ?coor . " +
+		"OPTIONAL { ?item wdt:P18 ?image } . " +
+		"SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\" } " +
+	    "} " +
+	"ORDER BY ?itemLabel"
+
+	let characterSet = NSMutableCharacterSet()
+	characterSet.formUnion(with: CharacterSet.urlQueryAllowed)
+	characterSet.removeCharacters(in: "?")
+	characterSet.removeCharacters(in: "&")
+	characterSet.removeCharacters(in: ":")
+
+	let encodedQuery = query.addingPercentEncoding(withAllowedCharacters: characterSet as CharacterSet)!
+
+	let request = URLRequest(url: URL(string: "https://query.wikidata.org/sparql?query=\(encodedQuery)&format=json")!)
+
+	URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+	    guard let data = data else { return }
+	    guard let json = try? JSONSerialization.jsonObject(with: data, options:[]) as? [String: AnyObject] else { return }
+	    guard let results = json?["results"] as? [String: AnyObject] else { return }
+	    guard let items = results["bindings"] as? [[String: AnyObject]] else { return }
+	    DispatchQueue.main.async {
+		completion(self.parseJSONItems(items: items))
+	    }
+	}).resume()
+    }
+
     func parseJSONItems(items: [[String: AnyObject]]) -> [MGLFeature] {
 	var features = [MGLFeature]()
 	for item in items {
@@ -123,6 +207,8 @@ class RuntimeMultipleAnnotationsExample_Swift: UIViewController, MGLMapViewDeleg
 	    let coordinate = CLLocationCoordinate2D(latitude: Double(pointComponents[1])!, longitude: Double(pointComponents[0])!)
 	    let feature = MGLPointFeature()
 	    feature.coordinate = coordinate
+	    feature.title = title
+	    // A feature's attributes can used by runtime styling for things like text labels
 	    feature.attributes = [
 		"name": title
 	    ]
