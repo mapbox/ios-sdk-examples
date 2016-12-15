6d9d6e673a40c9efc378504279ba4b273d1b930d
Nadia Barbosa <nadiabarbosa@me.com>
2016-12-14 17:32:51 -0800

    Oh wow I actually got something working

--- Examples/Swift 3/AnnotationViewMultipleExample.swift
+++ Examples/Swift 3/AnnotationViewMultipleExample.swift
@@ -11,7 +11,7 @@
     
     @objc(AnnotationViewMultipleExample_Swift)
     
-    class AnnotationViewMultipleExample_Swift: UIViewController {
+    class AnnotationViewMultipleExample_Swift: UIViewController, MGLMapViewDelegate {
         override func viewDidLoad() {
             super.viewDidLoad()
             
@@ -19,8 +19,41 @@
             mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
             
             // Set the map’s center coordinate and zoom level.
-            mapView.setCenter(CLLocationCoordinate2D(latitude: 59.31, longitude: 18.06), zoomLevel: 9, animated: false)
+            mapView.setCenter(CLLocationCoordinate2D(latitude: 39.83, longitude: -98.58), zoomLevel: 2, animated: false)
+            
             view.addSubview(mapView)
+            
+            mapView.delegate = self
+    
+            
+            struct annotationCollection {
+                let location: CLLocationCoordinate2D
+                let title: String
+                
+            }
+            
+            var myStruct1 = annotationCollection(location: CLLocationCoordinate2D(latitude: 37.79, longitude: -122.43), title: "Point A")
+            
+            var myStruct2 = annotationCollection(location: CLLocationCoordinate2D(latitude: 38.90, longitude: -77.04), title: "Point B")
+            
+            let myArray = [myStruct1, myStruct2]
+            
+            var pointAnnotations = [MGLPointAnnotation]()
+            
+            for item in myArray {
+                let point = MGLPointAnnotation()
+                point.coordinate = item.location
+                point.title = item.title
+                pointAnnotations.append(point)
+            }
+            
+            mapView.addAnnotations(pointAnnotations)
+            
         }
+        
+        func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+            return true
+        }
+        
     }
 #endif
