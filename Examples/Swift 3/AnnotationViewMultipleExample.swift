6f278daed26279327b365f8f5c0c67fc1a1fc51a
Nadia Barbosa <nadiabarbosa@me.com>
2016-12-15 17:00:44 -0800

    Clean up & comment

--- Examples/Swift 3/AnnotationViewMultipleExample.swift
+++ Examples/Swift 3/AnnotationViewMultipleExample.swift
@@ -11,6 +11,7 @@
     
     @objc(AnnotationViewMultipleExample_Swift)
     
+    // Example view controller
     class AnnotationViewMultipleExample_Swift: UIViewController, MGLMapViewDelegate {
         override func viewDidLoad() {
             super.viewDidLoad()
@@ -26,56 +27,61 @@
             
             mapView.delegate = self
     
-            
             struct annotationCollection {
                 let location: CLLocationCoordinate2D
                 let title: String
-                
             }
             
-            var myStruct1 = annotationCollection(location: CLLocationCoordinate2D(latitude: 37.79, longitude: -122.43), title: "Point A")
+            let pointA = annotationCollection(location: CLLocationCoordinate2D(latitude: 37.79, longitude: -122.43), title: "San Francisco")
             
-            var myStruct2 = annotationCollection(location: CLLocationCoordinate2D(latitude: 38.90, longitude: -77.04), title: "Point B")
+            let pointB = annotationCollection(location: CLLocationCoordinate2D(latitude: 38.90, longitude: -77.04), title: "Washington, D.C")
             
-            let myArray = [myStruct1, myStruct2]
+            let locations = [pointA, pointB]
             
+            // Fill an array with point annotations and add it to the map.
             var pointAnnotations = [MGLPointAnnotation]()
-            
-            for item in myArray {
+            for item in locations {
                 let point = MGLPointAnnotation()
                 point.coordinate = item.location
                 point.title = item.title
                 pointAnnotations.append(point)
             }
             
+            // Add all annotation to the map.
             mapView.addAnnotations(pointAnnotations)
             
         }
         
+        // This delegate method is where you tell the map to load a view for a specific annotation.
         func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? {
             
+            // This example is only concerned with point annotations.
             guard annotation is MGLPointAnnotation else {
                 return nil
             }
             
-        
+            // Use the point annotation’s longitude value (as a string) as the reuse identifier for its view.
             let reuseIdentifier = "\(annotation.coordinate.longitude)"
             
+            // For better performance, always try to reuse existing annotations.
             var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier)
             
+            // If there’s no reusable annotation view available, initialize a new one.
             if annotationView == nil {
                 annotationView = CustomAnnotationView(reuseIdentifier: reuseIdentifier)
                 annotationView!.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
                 
-                let hue : CGFloat = CGFloat(arc4random() % 256) / 256
+                // Generate a random number between 0 and 1
+                let randomHue : CGFloat = CGFloat(arc4random_uniform(101)) / 100.0
                 
-                annotationView!.backgroundColor = UIColor(hue: hue, saturation: 1, brightness: 1, alpha: 1)
+                annotationView!.backgroundColor = UIColor(hue: randomHue, saturation: 1, brightness: 1, alpha: 1)
             }
             
             return annotationView
         }
         
         func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+            // Always allow callouts to popup when annotations are tapped
             return true
         }
         
