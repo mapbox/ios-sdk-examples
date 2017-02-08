5550d7cfc40e0f65838120d1d0e541e3f23aa581
Jason Wray <jason@kulturny.com>
2017-02-07 19:18:07 -0500

    Fix SourceCustomVector indentation, formatting

--- Examples/ObjectiveC/SourceCustomVectorExample.m
+++ Examples/ObjectiveC/SourceCustomVectorExample.m
@@ -8,8 +8,12 @@ NSString *const MBXExampleSourceCustomVector = @"SourceCustomVectorExample";
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    // In this case we're using a custom style json (https://www.mapbox.com/mapbox-gl-style-spec/) to add a third party tile source: "https://vector.mapzen.com/osm/all/{z}/{x}/{y}.mvt
-    MGLMapView *mapView = [[MGLMapView alloc] initWithFrame:self.view.bounds styleURL:[[NSBundle mainBundle] URLForResource:@"third_party_vector_style" withExtension:@"json"]];
+    // Third party vector tile sources can be added.
+    
+    // In this case we're using custom style JSON (https://www.mapbox.com/mapbox-gl-style-spec/) to add a third party tile source: <https://vector.mapzen.com/osm/all/{z}/{x}/{y}.mvt>
+    NSURL *customStyleURL = [[NSBundle mainBundle] URLForResource:@"third_party_vector_style" withExtension:@"json"];
+
+    MGLMapView *mapView = [[MGLMapView alloc] initWithFrame:self.view.bounds styleURL:customStyleURL];
 
     mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
 
--- Examples/Swift/SourceCustomVectorExample.swift
+++ Examples/Swift/SourceCustomVectorExample.swift
@@ -3,18 +3,21 @@ import Mapbox
 @objc(SourceCustomVectorExample_Swift)
 
 class SourceCustomVectorExample_Swift: UIViewController {
-
     var mapView: MGLMapView!
 
     override func viewDidLoad() {
-	super.viewDidLoad()
+        super.viewDidLoad()
 
-	// Third party vector tile sources can be added
-	// In this case we're using a custom style json (https://www.mapbox.com/mapbox-gl-style-spec/) to add a third party tile source: "https://vector.mapzen.com/osm/all/{z}/{x}/{y}.mvt
-	mapView = MGLMapView(frame: view.bounds, styleURL: Bundle.main.url(forResource: "third_party_vector_style", withExtension: "json")!)
+        // Third party vector tile sources can be added.
+        
+        // In this case we're using custom style JSON (https://www.mapbox.com/mapbox-gl-style-spec/) to add a third party tile source: <https://vector.mapzen.com/osm/all/{z}/{x}/{y}.mvt>
+        let customStyleURL = Bundle.main.url(forResource: "third_party_vector_style", withExtension: "json")!
 
-	mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mapView = MGLMapView(frame: view.bounds, styleURL: customStyleURL)
 
-	view.addSubview(mapView)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        view.addSubview(mapView)
     }
 }
+
