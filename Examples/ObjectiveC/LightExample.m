8fbcae8a655b3d79371f81f2ae66bcc9ed5f5fdf
jmkiley <jordan.kiley@mapbox.com>
2017-06-26 11:28:20 -0700

    Added Objective-C

--- Examples/ObjectiveC/LightExample.m
+++ Examples/ObjectiveC/LightExample.m
@@ -7,8 +7,9 @@
 //
 
 #import "LightExample.h"
+@import Mapbox;
 
-@interface LightExample ()
+@interface LightExample () <MGLMapViewDelegate>
 
 @end
 
@@ -18,22 +19,50 @@ NSString *const MBXExampleLight = @"LightExample";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    MGLMapView *mapView = [[MGLMapView alloc] initWithFrame:self.view.bounds styleURL:[MGLStyle lightStyleURLWithVersion:9]];
+    mapView.delegate = self;
+    
+    // Center the map on the Flatiron Building in New York, NY.
+    mapView.camera = [MGLMapCamera cameraLookingAtCenterCoordinate:CLLocationCoordinate2DMake(40.7411, -73.9897) fromDistance:600 pitch:45 heading:200];
+    mapView.tintColor = [UIColor grayColor];
+    
+    [self.view addSubview:mapView];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)mapView:(MGLMapView *)mapView didFinishLoadingStyle:(MGLStyle *)style {
+    
+    // Add a MGLFillExtrusionStyleLayer.
+    [self addFillExtrusionLayer:style];
+    
+    // Create an MGLLight object.
+    MGLLight *light = [[MGLLight alloc] init];
+    
+    // Create an MGLSphericalPosition and set the radial, azimuthal, and polar values.
+    MGLSphericalPosition position = MGLSphericalPositionMake(5, 180, 80);
+    light.position = [MGLStyleValue valueWithRawValue:[NSValue valueWithMGLSphericalPosition:position]];
+    
+    // Set the light anchor to the map and add the light object to the map view's style.
+    light.anchor = [MGLStyleValue valueWithRawValue:[NSValue valueWithMGLLightAnchor:MGLLightAnchorMap]];
+    style.light = light;
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)addFillExtrusionLayer:(MGLStyle *)style {
+    // Access the Mapbox Streets source and use it to create a `MGLFillExtrusionStyleLayer`. The source identifier is `composite`. Use the `sources` property on a style to verify source identifiers.
+    MGLSource *source = [style sourceWithIdentifier:@"composite"];
+    MGLFillExtrusionStyleLayer *layer = [[MGLFillExtrusionStyleLayer alloc] initWithIdentifier:@"extrusion-layer" source:source];
+    layer.sourceLayerIdentifier = @"building";
+    layer.fillExtrusionBase = [MGLStyleValue valueWithInterpolationMode:MGLInterpolationModeIdentity
+                                                            sourceStops:nil
+                                                          attributeName:@"min_height"
+                                                                options:nil];
+    layer.fillExtrusionHeight = [MGLStyleValue valueWithInterpolationMode:MGLInterpolationModeIdentity sourceStops:nil attributeName:@"height" options:nil];
+    layer.fillExtrusionOpacity = [MGLStyleValue valueWithRawValue:@0.75];
+    layer.fillExtrusionColor = [MGLStyleValue valueWithRawValue:[UIColor whiteColor]];
+    MGLStyleLayer *symbolLayer = [style layerWithIdentifier:@"poi-scalerank3"];
+    [style insertLayer:layer belowLayer:symbolLayer];
+    
 }
-*/
-
 @end
--- Examples/Swift/LightExample.swift
+++ Examples/Swift/LightExample.swift
@@ -8,7 +8,7 @@ class LightExample: UIViewController, MGLMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let mapView = MGLMapView(frame: view.bounds, styleURL: MGLStyle.streetsStyleURL(withVersion: 10))
+        let mapView = MGLMapView(frame: view.bounds, styleURL: MGLStyle.streetsStyleURL(withVersion: 9))
         mapView.delegate = self
         
         // Center the map on the Flatiron Building in New York, NY.
@@ -20,22 +20,8 @@ class LightExample: UIViewController, MGLMapViewDelegate {
     
     func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle)
     {
-        
-        // Access the Mapbox Streets source and use it to create a `MGLFillExtrusionStyleLayer`. The source identifier is `composite`. Use the `sources` property on a style to verify source identifiers.
-        if let source = style.source(withIdentifier: "composite") {
-            let layer = MGLFillExtrusionStyleLayer(identifier: "extrusion-layer", source: source)
-            layer.sourceLayerIdentifier = "building"
-            layer.fillExtrusionBase = nil
-            layer.fillExtrusionBase = MGLStyleValue(interpolationMode: .identity, sourceStops: nil, attributeName: "min_height", options: nil)
-            layer.fillExtrusionHeight = MGLStyleValue(interpolationMode: .identity, sourceStops: nil, attributeName: "height", options: nil)
-            layer.fillExtrusionOpacity = MGLStyleValue(rawValue: 0.75)
-            layer.fillExtrusionColor = MGLStyleValue(rawValue: .white)
-            if let symbolLayer = style.layer(withIdentifier: "poi-scalerank3") {
-                style.insertLayer(layer, below: symbolLayer)
-            } else {
-                style.addLayer(layer)
-            }
-        }
+        // Add a MGLFillExtrusionStyleLayer.
+        addFillExtrusionLayer(style: style)
         
         // Create an MGLLight object.
         let light = MGLLight()
@@ -47,5 +33,22 @@ class LightExample: UIViewController, MGLMapViewDelegate {
         // Set the light anchor to the map and add the light object to the map view's style.
         light.anchor = MGLStyleValue(rawValue: NSValue(mglLightAnchor: MGLLightAnchor.map))
         style.light = light
+    }
+    
+    func addFillExtrusionLayer(style: MGLStyle) {
+        // Access the Mapbox Streets source and use it to create a `MGLFillExtrusionStyleLayer`. The source identifier is `composite`. Use the `sources` property on a style to verify source identifiers.
+        if let source = style.source(withIdentifier: "composite") {
+            let layer = MGLFillExtrusionStyleLayer(identifier: "extrusion-layer", source: source)
+            layer.sourceLayerIdentifier = "building"
+            layer.fillExtrusionBase = MGLStyleValue(interpolationMode: .identity, sourceStops: nil, attributeName: "min_height", options: nil)
+            layer.fillExtrusionHeight = MGLStyleValue(interpolationMode: .identity, sourceStops: nil, attributeName: "height", options: nil)
+            layer.fillExtrusionOpacity = MGLStyleValue(rawValue: 0.75)
+            layer.fillExtrusionColor = MGLStyleValue(rawValue: .white)
+            if let symbolLayer = style.layer(withIdentifier: "poi-scalerank3") {
+                style.insertLayer(layer, below: symbolLayer)
+            } else {
+                style.addLayer(layer)
+            }
+        }
     }
 }
