cbde9afcac5cedf979b04c5e91c265b4ae9fbbd0
Jason Wray <jason@kulturny.com>
2017-08-01 15:43:19 -0400

    Refactor tap gesture in point conversion example

--- Examples/ObjectiveC/PointConversionExample.m
+++ Examples/ObjectiveC/PointConversionExample.m
@@ -17,14 +17,13 @@ NSString *const MBXExamplePointConversion = @"PointConversionExample";
 
     [self.view addSubview:self.mapView];
 
-    // Double tapping zooms the map, so ensure that can still happen.
-    UITapGestureRecognizer *doubleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:nil];
-    doubleTap.numberOfTapsRequired = 2;
-    [self.mapView addGestureRecognizer:doubleTap];
-
-    // Delay single tap recognition until it is clearly not a double.
+    // Add a single tap gesture recognizer. This gesture requires the built-in MGLMapView tap gestures (such as those for zoom and annotation selection) to fail.
     UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleSingleTap:)];
-    [singleTap requireGestureRecognizerToFail:doubleTap];
+    for (UIGestureRecognizer *recognizer in self.mapView.gestureRecognizers) {
+        if ([recognizer isKindOfClass:[UITapGestureRecognizer class]]) {
+            [singleTap requireGestureRecognizerToFail:recognizer];
+        }
+    }
     [self.mapView addGestureRecognizer:singleTap];
 
     // Convert `mapView.centerCoordinate` (CLLocationCoordinate2D)
@@ -39,23 +38,25 @@ NSString *const MBXExamplePointConversion = @"PointConversionExample";
 
 - (void)handleSingleTap:(UITapGestureRecognizer *)tap {
     // Convert tap location (CGPoint)
-    // to geographic coordinates (CLLocationCoordinate2D).
-    CLLocationCoordinate2D location = [self.mapView convertPoint:[tap locationInView:self.mapView]
+    // to geographic coordinate (CLLocationCoordinate2D).
+    CLLocationCoordinate2D tapCoordinate = [self.mapView convertPoint:[tap locationInView:self.mapView]
                                             toCoordinateFromView:self.mapView];
 
-    NSLog(@"You tapped at: %.5f, %.5f", location.latitude, location.longitude);
+    NSLog(@"You tapped at: %.5f, %.5f", tapCoordinate.latitude, tapCoordinate.longitude);
 
-    // Create an array of coordinates for our polyline.
+    // Create an array of coordinates for our polyline, starting at the center of the map and ending at the tap coordinate.
     CLLocationCoordinate2D coordinates[] = {
         self.mapView.centerCoordinate,
-        location
+        tapCoordinate
     };
     NSUInteger numberOfCoordinates = sizeof(coordinates) / sizeof(CLLocationCoordinate2D);
 
-    // Remove existing polyline from the map, (re)add polyline with coordinates.
+    // Remove any existing polyline(s) from the map.
     if (self.mapView.annotations.count) {
         [self.mapView removeAnnotations:self.mapView.annotations];
     }
+
+    // Add a polyline with the new coordinates.
     MGLPolyline *polyline = [MGLPolyline polylineWithCoordinates:coordinates
                                                            count:numberOfCoordinates];
     [self.mapView addAnnotation:polyline];
--- Examples/Swift/PointConversionExample.swift
+++ Examples/Swift/PointConversionExample.swift
@@ -12,39 +12,36 @@ class PointConversionExample_Swift: UIViewController, MGLMapViewDelegate {
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         
         view.addSubview(mapView)
-        
-        // double tapping zooms the map, so ensure that can still happen
-        let doubleTap = UITapGestureRecognizer(target: self, action: nil)
-        doubleTap.numberOfTapsRequired = 2
-        mapView.addGestureRecognizer(doubleTap)
-        
-        // delay single tap recognition until it is clearly not a double
-        let singleTap = UITapGestureRecognizer(target: self, action: #selector(handleSingleTap))
-        singleTap.require(toFail: doubleTap)
+
+        // Add a single tap gesture recognizer. This gesture requires the built-in MGLMapView tap gestures (such as those for zoom and annotation selection) to fail.
+        let singleTap = UITapGestureRecognizer(target: self, action: #selector(handleSingleTap(tap:)))
+        for recognizer in mapView.gestureRecognizers! where recognizer is UITapGestureRecognizer {
+            singleTap.require(toFail: recognizer)
+        }
         mapView.addGestureRecognizer(singleTap)
-        
-        // convert `mapView.centerCoordinate` (CLLocationCoordinate2D)
-        // to screen location (CGPoint)
+
+        // Convert `mapView.centerCoordinate` (CLLocationCoordinate2D)
+        // to screen location (CGPoint).
         let centerScreenPoint: CGPoint = mapView.convert(mapView.centerCoordinate, toPointTo: mapView)
         print("Screen center: \(centerScreenPoint) = \(mapView.center)")
     }
     
     func handleSingleTap(tap: UITapGestureRecognizer) {
-        // convert tap location (CGPoint)
-        // to geographic coordinates (CLLocationCoordinate2D)
-        let location: CLLocationCoordinate2D = mapView.convert(tap.location(in: mapView), toCoordinateFrom: mapView)
-        print("You tapped at: \(location.latitude), \(location.longitude)")
+        // Convert tap location (CGPoint)
+        // to geographic coordinate (CLLocationCoordinate2D).
+        let tapCoordinate: CLLocationCoordinate2D = mapView.convert(tap.location(in: mapView), toCoordinateFrom: mapView)
+        print("You tapped at: \(tapCoordinate.latitude), \(tapCoordinate.longitude)")
         
-        // create an array of coordinates for our polyline
-        var coordinates: [CLLocationCoordinate2D] = [mapView.centerCoordinate, location]
+        // Create an array of coordinates for our polyline, starting at the center of the map and ending at the tap coordinate.
+        var coordinates: [CLLocationCoordinate2D] = [mapView.centerCoordinate, tapCoordinate]
         
-        // remove existing polyline from the map, (re)add polyline with coordinates
-        if (mapView.annotations?.count != nil) {
-            mapView.removeAnnotations(mapView.annotations!)
+        // Remove any existing polyline(s) from the map.
+        if mapView.annotations?.count != nil, let existingAnnotations = mapView.annotations {
+            mapView.removeAnnotations(existingAnnotations)
         }
 
+        // Add a polyline with the new coordinates.
         let polyline = MGLPolyline(coordinates: &coordinates, count: UInt(coordinates.count))
-
         mapView.addAnnotation(polyline)
     }
 }
