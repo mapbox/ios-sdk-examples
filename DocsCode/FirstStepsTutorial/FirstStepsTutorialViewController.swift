70667073770c73152fdf1068077ea6e948cc72eb
Nadia Barbosa <captainbarbosa@users.noreply.github.com>
2017-10-20 22:18:53 -0400

    Update magic comment syntax on first steps tutorial

--- DocsCode/FirstStepsTutorial/FirstStepsTutorialViewController.swift
+++ DocsCode/FirstStepsTutorial/FirstStepsTutorialViewController.swift
@@ -10,11 +10,11 @@ class FirstStepsTutorialViewController: UIViewController, MGLMapViewDelegate {
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         mapView.setCenter(CLLocationCoordinate2D(latitude: 40.74699, longitude: -73.98742), zoomLevel: 9, animated: false)
         view.addSubview(mapView)
-        // #-end-code-snippet
+        // #-end-code-snippet: first-steps-ios-sdk initialize-map-swift
         
         // #-code-snippet: first-steps-ios-sdk change-style-swift
         mapView.styleURL = MGLStyle.satelliteStyleURL()
-        // #-end-code-snippet
+        // #-end-code-snippet: first-steps-ios-sdk change-style-swift
         
         // #-code-snippet: first-steps-ios-sdk add-annotation-swift
         // Add a point annotation
@@ -23,24 +23,23 @@ class FirstStepsTutorialViewController: UIViewController, MGLMapViewDelegate {
         annotation.title = "Central Park"
         annotation.subtitle = "The biggest park in New York City!"
         mapView.addAnnotation(annotation)
-        // #-end-code-snippet
-        
-        // #-code-snippet: first-steps-ios-sdk show-location-swift
-        // Allow the map view to display the user's location
-        mapView.showsUserLocation = true
-        // #-end-code-snippet
+        // #-end-code-snippet: first-steps-ios-sdk add-annotation-swift
         
         // #-code-snippet: first-steps-ios-sdk set-delegate-swift
         // Set the map view's delegate
         mapView.delegate = self
-        // #-end-code-snippet
+        // #-end-code-snippet: first-steps-ios-sdk set-delegate-swift
         
+        // #-code-snippet: first-steps-ios-sdk show-location-swift
+        // Allow the map view to display the user's location
+        mapView.showsUserLocation = true
+        // #-end-code-snippet: first-steps-ios-sdk show-location-swift
     }
     
-    // #-code-snippet: first-steps-ios-sdk add-callout-swift
+    // #-code-snippet: first-steps-ios-sdk can-show-callout-swift
     func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
         // Always allow callouts to popup when annotations are tapped.
         return true
     }
-    // #-end-code-snippet
+    // #-end-code-snippet: first-steps-ios-sdk can-show-callout-swift
 }
