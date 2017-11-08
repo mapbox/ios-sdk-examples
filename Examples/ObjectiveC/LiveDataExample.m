f14f56d993a555cf9938a3f9aa5d1a734f244e47
Jordan Kiley <jmkiley@users.noreply.github.com>
2017-11-08 10:58:14 -0800

    Add example for live data (#121)

---
+++ Examples/ObjectiveC/LiveDataExample.m
@@ -0,0 +1,54 @@
+
+#import "LiveDataExample.h"
+@import Mapbox;
+
+NSString *const MBXExampleLiveData = @"LiveDataExample";
+
+@interface LiveDataExample () <MGLMapViewDelegate>
+
+@property (nonatomic, strong, nullable) NSTimer *timer;
+@property MGLShapeSource *source;
+@end
+
+@implementation LiveDataExample
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Create a new map view using the Mapbox Dark style.
+    MGLMapView *mapView = [[MGLMapView alloc] initWithFrame:self.view.bounds styleURL:[MGLStyle darkStyleURL]];
+    mapView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+    mapView.tintColor = [UIColor darkGrayColor];
+    
+    // Set the map view‘s delegate property.
+    mapView.delegate = self;
+    
+    [self.view addSubview:mapView];
+}
+
+- (void)mapView:(MGLMapView *)mapView didFinishLoadingStyle:(MGLStyle *)style {
+    // Add a source to the map. https://wanderdrone.appspot.com/ generates coordinates for simulated paths.
+    NSURL *url = [NSURL URLWithString:@"https://wanderdrone.appspot.com/"];
+    _source = [[MGLShapeSource alloc] initWithIdentifier:@"wanderdrone" URL:url options:nil];
+    [style addSource:_source];
+    
+    // Add a Maki icon to the map to represent the drone's coordinate. The specified icon is included in the Mapbox Dark style's sprite sheet. For more information about Maki icons, see https://www.mapbox.com/maki-icons/
+    MGLSymbolStyleLayer *droneLayer = [[MGLSymbolStyleLayer alloc] initWithIdentifier:@"wanderdrone" source:_source];
+    droneLayer.iconImageName = [MGLStyleValue valueWithRawValue:@"rocket-15"];
+    [style addLayer:droneLayer];
+    
+    // Create a timer that calls the `updateUrl` function every 1.5 seconds.
+    [_timer invalidate];
+    _timer = [NSTimer scheduledTimerWithTimeInterval:1.5 target:self selector:@selector(updateURL) userInfo:nil repeats:YES];
+}
+
+- (void)updateURL {
+    // Update the icon's position by setting the `url` property on the source.
+    _source.URL = _source.URL;
+}
+- (void)viewWillDisappear:(BOOL)animated {
+    // Invalidate the timer if the view will disappear.
+    [_timer invalidate];
+    _timer = nil;
+}
+@end
--- Podfile
+++ Podfile
@@ -2,7 +2,6 @@ platform :ios, '8.0'
 use_frameworks!
 
 def shared_pods
-  #pod 'Mapbox-iOS-SDK', '~> 3.6.3'
   pod 'Mapbox-iOS-SDK-symbols', :podspec => 'https://raw.githubusercontent.com/mapbox/mapbox-gl-native/ios-v3.6.4/platform/ios/Mapbox-iOS-SDK-symbols.podspec'
 end
 
