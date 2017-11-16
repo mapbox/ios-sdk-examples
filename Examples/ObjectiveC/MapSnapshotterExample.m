7b27ff5aebf611bda5e9698786be76b41eeee273
jmkiley <jordan.kiley@mapbox.com>
2017-11-16 09:49:55 -0800

    Added comments, bumped podfile

--- Examples/ObjectiveC/MapSnapshotterExample.m
+++ Examples/ObjectiveC/MapSnapshotterExample.m
@@ -12,17 +12,20 @@ NSString *const MBXExampleMapSnapshotter = @"MapSnapshotterExample";
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    // Create a UIImageView that will store the map snapshot.
     UIImageView *imageView = [[UIImageView alloc] initWithFrame:self.view.bounds];
     imageView.backgroundColor = [UIColor blackColor];
     [self.view addSubview:imageView];
+    
     // Center map on the Giza Pyramid Complex in Egypt.
     CLLocationCoordinate2D center = CLLocationCoordinate2DMake(29.9773, 31.1325);
     MGLMapCamera *camera = [MGLMapCamera cameraLookingAtCenterCoordinate:center fromDistance:0 pitch:0 heading:0];
     
-    
+    // Set the snapshot object's style, camera, size, and zoom level.
     MGLMapSnapshotOptions *options = [[MGLMapSnapshotOptions alloc] initWithStyleURL:[MGLStyle satelliteStreetsStyleURL] camera:camera size:self.view.bounds.size];
     options.zoomLevel = 14;
     
+    // Create the map snapshot.
     MGLMapSnapshotter *snapshotter = [[MGLMapSnapshotter alloc] initWithOptions:options];
     
     [snapshotter startWithCompletionHandler:^(MGLMapSnapshot * _Nullable snapshot, NSError * _Nullable error) {
--- Examples/Swift/MapSnapshotterExample.swift
+++ Examples/Swift/MapSnapshotterExample.swift
@@ -7,15 +7,20 @@ class MapSnapshotterExample: UIViewController, MGLMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Create a UIImageView that will store the map snapshot.
         let imageView = UIImageView(frame: view.bounds)
         imageView.backgroundColor = .black
         view.addSubview(imageView)
+        
         // Center map on the Giza Pyramid Complex in Egypt.
         let center = CLLocationCoordinate2D(latitude: 29.9773, longitude: 31.1325)
         let camera = MGLMapCamera(lookingAtCenter: center, fromDistance: 0, pitch: 0, heading: 0)
         
+        // Set the snapshot object's style, camera, size, and zoom level.
         let options = MGLMapSnapshotOptions(styleURL: MGLStyle.satelliteStreetsStyleURL(), camera: camera, size: view.bounds.size)
         options.zoomLevel = 14
+        
+        // Create the map snapshot.
         let snapshotter = MGLMapSnapshotter(options: options)
         
         snapshotter.start { (snapshot, error) in
