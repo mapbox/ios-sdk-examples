aaf00e6b783ba9272cfbb8f1bc283757ac41b955
jmkiley <jordan.kiley@mapbox.com>
2017-11-16 15:11:31 -0800

    Added button

--- Examples/ObjectiveC/MapSnapshotterExample.m
+++ Examples/ObjectiveC/MapSnapshotterExample.m
@@ -4,6 +4,9 @@
 
 @interface MapSnapshotterExample ()
 
+@property MGLMapView *mapView;
+@property UIButton *button;
+
 @end
 
 NSString *const MBXExampleMapSnapshotter = @"MapSnapshotterExample";
@@ -12,19 +15,31 @@ NSString *const MBXExampleMapSnapshotter = @"MapSnapshotterExample";
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    _mapView = [[MGLMapView alloc] initWithFrame:self.view.bounds styleURL:[MGLStyle satelliteStreetsStyleURL]];
+    _mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    
+    // Center map on the Giza Pyramid Complex in Egypt.
+    CLLocationCoordinate2D center = CLLocationCoordinate2DMake(29.9773, 31.1325);
+    [_mapView setCenterCoordinate:center zoomLevel:14 animated:NO];
+    [self.view addSubview:_mapView];
+    
+    // Create a button to take a map snapshot.
+    _button = [[UIButton alloc] initWithFrame:CGRectMake(self.view.bounds.size.width / 2 - 15, self.view.bounds.size.height - 80, 30, 30)];
+    _button.layer.cornerRadius = 15;
+    [_button setImage:[UIImage imageNamed:@"camera"] forState:UIControlStateNormal];
+    [_button addTarget:self action:@selector(createSnapshot) forControlEvents:UIControlEventAllTouchEvents];
+    [self.view addSubview:_button];
+}
+
+- (void)createSnapshot {
     // Create a UIImageView that will store the map snapshot.
     UIImageView *imageView = [[UIImageView alloc] initWithFrame:self.view.bounds];
     imageView.backgroundColor = [UIColor blackColor];
     imageView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    [self.view addSubview:imageView];
-    
-    // Center map on the Giza Pyramid Complex in Egypt.
-    CLLocationCoordinate2D center = CLLocationCoordinate2DMake(29.9773, 31.1325);
-    MGLMapCamera *camera = [MGLMapCamera cameraLookingAtCenterCoordinate:center fromDistance:0 pitch:0 heading:0];
-    
-    // Set the snapshot object's style, camera, size, and zoom level.
-    MGLMapSnapshotOptions *options = [[MGLMapSnapshotOptions alloc] initWithStyleURL:[MGLStyle satelliteStreetsStyleURL] camera:camera size:self.view.bounds.size];
-    options.zoomLevel = 14;
+
+    // Use the map view's style, camera, size, and zoom level to set the snapshot's options.
+    MGLMapSnapshotOptions *options = [[MGLMapSnapshotOptions alloc] initWithStyleURL:_mapView.styleURL camera:_mapView.camera size:self.view.bounds.size];
+    options.zoomLevel = _mapView.zoomLevel;
     
     // Create the map snapshot.
     MGLMapSnapshotter *snapshotter = [[MGLMapSnapshotter alloc] initWithOptions:options];
@@ -34,8 +49,8 @@ NSString *const MBXExampleMapSnapshotter = @"MapSnapshotterExample";
             NSLog(@"Unable to create a map snapshot.");
         } else if (snapshot != nil) {
             imageView.image = snapshot.image;
+            [self.view addSubview:imageView];
         }
     }];
 }
-
 @end
--- Examples/Swift/MapSnapshotterExample.swift
+++ Examples/Swift/MapSnapshotterExample.swift
@@ -4,22 +4,37 @@ import Mapbox
 
 class MapSnapshotterExample: UIViewController, MGLMapViewDelegate {
 
+    var mapView : MGLMapView!
+    var button : UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mapView = MGLMapView(frame: view.bounds, styleURL: MGLStyle.satelliteStreetsStyleURL())
+        mapView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        
+        // Center map on the Giza Pyramid Complex in Egypt.
+        let center = CLLocationCoordinate2D(latitude: 29.9773, longitude: 31.1325)
+        mapView.setCenter(center, zoomLevel: 14, animated: false)
+        view.addSubview(mapView)
+
+        // Create a button to take a map snapshot.
+        button = UIButton(frame: CGRect(x: view.bounds.width / 2 - 15, y: view.bounds.height - 80, width: 30, height: 30))
+        button.layer.cornerRadius = 15
+        button.setImage(UIImage(named: "camera"), for: .normal)
+        button.addTarget(self, action: #selector(createSnapshot), for: .allTouchEvents)
+        view.addSubview(button)
+    }
+    
+    @objc func createSnapshot() {
         
         // Create a UIImageView that will store the map snapshot.
         let imageView = UIImageView(frame: view.bounds)
         imageView.backgroundColor = .black
         imageView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        view.addSubview(imageView)
         
-        // Center map on the Giza Pyramid Complex in Egypt.
-        let center = CLLocationCoordinate2D(latitude: 29.9773, longitude: 31.1325)
-        let camera = MGLMapCamera(lookingAtCenter: center, fromDistance: 0, pitch: 0, heading: 0)
-        
-        // Set the snapshot object's style, camera, size, and zoom level.
-        let options = MGLMapSnapshotOptions(styleURL: MGLStyle.satelliteStreetsStyleURL(), camera: camera, size: view.bounds.size)
-        options.zoomLevel = 14
+        // Use the map view's style, camera, size, and zoom level to set the snapshot's options.
+        let options = MGLMapSnapshotOptions(styleURL: mapView.styleURL, camera: mapView.camera, size: mapView.bounds.size)
+        options.zoomLevel = mapView.zoomLevel
         
         // Create the map snapshot.
         let snapshotter = MGLMapSnapshotter(options: options)
@@ -29,6 +44,7 @@ class MapSnapshotterExample: UIViewController, MGLMapViewDelegate {
                 print("Unable to create a map snapshot.")
             } else if let snapshot = snapshot {
                 imageView.image = snapshot.image
+                self.view.addSubview(imageView)
             }
         }
     }
