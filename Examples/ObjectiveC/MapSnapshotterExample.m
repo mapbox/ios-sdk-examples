4dc04b2b2e44dc9f734320c6f05b3abf0e2150db
jmkiley <jordan.kiley@mapbox.com>
2017-11-17 10:43:30 -0800

    Addressed @friedbunny's feedback

--- Examples/ObjectiveC/MapSnapshotterExample.m
+++ Examples/ObjectiveC/MapSnapshotterExample.m
@@ -6,6 +6,7 @@
 
 @property MGLMapView *mapView;
 @property UIButton *button;
+@property UIImageView *imageView;
 
 @end
 
@@ -15,7 +16,7 @@ NSString *const MBXExampleMapSnapshotter = @"MapSnapshotterExample";
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    _mapView = [[MGLMapView alloc] initWithFrame:self.view.bounds styleURL:[MGLStyle satelliteStreetsStyleURL]];
+    _mapView = [[MGLMapView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height / 2) styleURL:[MGLStyle satelliteStreetsStyleURL]];
     _mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     
     // Center map on the Giza Pyramid Complex in Egypt.
@@ -24,19 +25,22 @@ NSString *const MBXExampleMapSnapshotter = @"MapSnapshotterExample";
     [self.view addSubview:_mapView];
     
     // Create a button to take a map snapshot.
-    _button = [[UIButton alloc] initWithFrame:CGRectMake(self.view.bounds.size.width / 2 - 15, self.view.bounds.size.height - 80, 30, 30)];
+    _button = [[UIButton alloc] initWithFrame:CGRectMake(_mapView.bounds.size.width / 2 - 15, _mapView.bounds.size.height - 40, 80, 30)];
     _button.layer.cornerRadius = 15;
+    _button.backgroundColor = [UIColor colorWithRed:0.96 green:0.65 blue:0.14 alpha:1.0];
     [_button setImage:[UIImage imageNamed:@"camera"] forState:UIControlStateNormal];
-    [_button addTarget:self action:@selector(createSnapshot) forControlEvents:UIControlEventAllTouchEvents];
+    [_button addTarget:self action:@selector(createSnapshot) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:_button];
+    
+    // Create a UIImageView that will store the map snapshot.
+    _imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, self.view.bounds.size.height / 2, self.view.bounds.size.width, self.view.bounds.size.height / 2)];
+    _imageView.backgroundColor = [UIColor blackColor];
+    _imageView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [self.view addSubview:_imageView];
 }
 
 - (void)createSnapshot {
-    // Create a UIImageView that will store the map snapshot.
-    UIImageView *imageView = [[UIImageView alloc] initWithFrame:self.view.bounds];
-    imageView.backgroundColor = [UIColor blackColor];
-    imageView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-
+    
     // Use the map's style, camera, size, and zoom level to set the snapshot's options.
     MGLMapSnapshotOptions *options = [[MGLMapSnapshotOptions alloc] initWithStyleURL:_mapView.styleURL camera:_mapView.camera size:self.view.bounds.size];
     options.zoomLevel = _mapView.zoomLevel;
@@ -48,8 +52,8 @@ NSString *const MBXExampleMapSnapshotter = @"MapSnapshotterExample";
         if (error != nil) {
             NSLog(@"Unable to create a map snapshot.");
         } else if (snapshot != nil) {
-            imageView.image = snapshot.image;
-            [self.view addSubview:imageView];
+            // Add the map snapshot's image to the image view.
+            _imageView.image = snapshot.image;
         }
     }];
 }
--- Examples/Swift/MapSnapshotterExample.swift
+++ Examples/Swift/MapSnapshotterExample.swift
@@ -6,10 +6,12 @@ class MapSnapshotterExample: UIViewController, MGLMapViewDelegate {
 
     var mapView : MGLMapView!
     var button : UIButton!
+    var imageView : UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        mapView = MGLMapView(frame: view.bounds, styleURL: MGLStyle.satelliteStreetsStyleURL())
+        mapView = MGLMapView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height / 2), styleURL: MGLStyle.satelliteStreetsStyleURL())
         mapView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
         
         // Center map on the Giza Pyramid Complex in Egypt.
@@ -18,33 +20,35 @@ class MapSnapshotterExample: UIViewController, MGLMapViewDelegate {
         view.addSubview(mapView)
 
         // Create a button to take a map snapshot.
-        button = UIButton(frame: CGRect(x: view.bounds.width / 2 - 15, y: view.bounds.height - 80, width: 30, height: 30))
+        button = UIButton(frame: CGRect(x: mapView.bounds.width / 2 - 40, y: mapView.bounds.height - 40, width: 80, height: 30))
         button.layer.cornerRadius = 15
+        button.backgroundColor = UIColor(red:0.96, green:0.65, blue:0.14, alpha:1.0)
         button.setImage(UIImage(named: "camera"), for: .normal)
-        button.addTarget(self, action: #selector(createSnapshot), for: .allTouchEvents)
+        button.addTarget(self, action: #selector(createSnapshot), for: .touchUpInside)
         view.addSubview(button)
+        
+        
+        // Create a UIImageView that will store the map snapshot.
+        imageView = UIImageView(frame: CGRect(x: 0, y: view.bounds.height / 2, width: view.bounds.width, height: view.bounds.height / 2))
+        imageView.backgroundColor = .black
+        imageView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.view.addSubview(imageView)
     }
     
     @objc func createSnapshot() {
-        
-        // Create a UIImageView that will store the map snapshot.
-        let imageView = UIImageView(frame: view.bounds)
-        imageView.backgroundColor = .black
-        imageView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        
+
         // Use the map's style, camera, size, and zoom level to set the snapshot's options.
         let options = MGLMapSnapshotOptions(styleURL: mapView.styleURL, camera: mapView.camera, size: mapView.bounds.size)
         options.zoomLevel = mapView.zoomLevel
         
         // Create the map snapshot.
         let snapshotter = MGLMapSnapshotter(options: options)
-        
         snapshotter.start { (snapshot, error) in
             if error != nil {
                 print("Unable to create a map snapshot.")
             } else if let snapshot = snapshot {
-                imageView.image = snapshot.image
-                self.view.addSubview(imageView)
+                // Add the map snapshot's image to the image view.
+                self.imageView.image = snapshot.image
             }
         }
     }
