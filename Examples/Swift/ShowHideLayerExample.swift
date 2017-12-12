a31584f33905d6604f47f2c874789a79dc66bc2e
Nadia Barbosa <captainbarbosa@users.noreply.github.com>
2017-12-12 17:52:32 -0500

    Remove class reference in selector for toggle layer example

--- Examples/Swift/ShowHideLayerExample.swift
+++ Examples/Swift/ShowHideLayerExample.swift
@@ -74,7 +74,7 @@ class ShowHideLayerExample_Swift: UIViewController, MGLMapViewDelegate {
         button.sizeToFit()
         button.center.x = self.view.center.x
         button.frame = CGRect(origin: CGPoint(x: button.frame.origin.x, y: self.view.frame.size.height - button.frame.size.height - 5), size: button.frame.size)
-        button.addTarget(self, action: #selector(ShowHideLayerExample_Swift.toggleLayer(sender:)), for: .touchUpInside)
+        button.addTarget(self, action: #selector(toggleLayer(sender:)), for: .touchUpInside)
         self.view.addSubview(button)
     }
 }
