e0819f631b35d3179504a1c8abd05fdadb344540
Nadia Barbosa <captainbarbosa@users.noreply.github.com>
2018-01-18 18:43:13 -0500

    Update third party vector source example (#142)
    * Change vector tile source URL + restyle
    
    * Add Mapillary attribution
    
    * SF -> Helsinki
    
    * Indent
    
    * Add link to attribution

--- Examples/ObjectiveC/ThirdPartyVectorStyleExample.m
+++ Examples/ObjectiveC/ThirdPartyVectorStyleExample.m
@@ -10,11 +10,11 @@ NSString *const MBXExampleThirdPartyVectorStyle = @"ThirdPartyVectorStyleExample
 
     // Third party vector tile sources can be added.
     
-    // In this case we're using custom style JSON (https://www.mapbox.com/mapbox-gl-style-spec/) to add a third party tile source: <https://vector.mapzen.com/osm/all/{z}/{x}/{y}.mvt>
+    // In this case we're using custom style JSON (https://www.mapbox.com/mapbox-gl-style-spec/) to add a third party tile source from Mapillary: <https://d25uarhxywzl1j.cloudfront.net/v0.1/{z}/{x}/{y}.mvt>
     NSURL *customStyleURL = [[NSBundle mainBundle] URLForResource:@"third_party_vector_style" withExtension:@"json"];
 
     MGLMapView *mapView = [[MGLMapView alloc] initWithFrame:self.view.bounds styleURL:customStyleURL];
-
+    [mapView setCenterCoordinate:CLLocationCoordinate2DMake(60.16, 24.93) zoomLevel:12 animated:NO];
     mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     mapView.tintColor = [UIColor whiteColor];
 
