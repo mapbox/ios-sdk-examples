814064e01d8e99f8e3d9e58c10cbd25397ca425d
jmkiley <jordan.kiley@mapbox.com>
2018-02-19 16:12:04 -0800

    started swift existed

--- Examples/Swift/MultipleImagesExample.swift
+++ Examples/Swift/MultipleImagesExample.swift
@@ -8,7 +8,8 @@ class MultipleImagesExample: UIViewController, MGLMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let mapView = MGLMapView(frame: view.bounds)
+        let mapView = MGLMapView(frame: view.bounds, styleURL: MGLStyle.outdoorsStyleURL())
+        mapView.setCenter(CLLocationCoordinate2D(latitude: 37.7456, longitude: -119.5936), zoomLevel: 14, animated: false)
         mapView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
         mapView.delegate = self
         view.addSubview(mapView)
