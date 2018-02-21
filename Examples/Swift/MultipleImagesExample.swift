05ca20163324f988800a876c9ec25141c1e75fab
jmkiley <jordan.kiley@mapbox.com>
2018-02-21 11:45:50 -0800

    Started Swift example

--- Examples/Swift/MultipleImagesExample.swift
+++ Examples/Swift/MultipleImagesExample.swift
@@ -9,10 +9,23 @@ class MultipleImagesExample: UIViewController, MGLMapViewDelegate {
         super.viewDidLoad()
 
         let mapView = MGLMapView(frame: view.bounds, styleURL: MGLStyle.outdoorsStyleURL())
-        mapView.setCenter(CLLocationCoordinate2D(latitude: 37.7456, longitude: -119.5936), zoomLevel: 14, animated: false)
+        mapView.setCenter(CLLocationCoordinate2D(latitude: 60.0438, longitude: -149.8164), zoomLevel: 10, animated: false)
         mapView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
         mapView.delegate = self
         view.addSubview(mapView)
+    }
+    
+    func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
+        if let url = URL(string: "mapbox://jordankiley.9v2hldnp") {
+            let source = MGLVectorSource(identifier: "park-feature-points", configurationURL: url)
+            style.addSource(source)
+            
+            let layer = MGLSymbolStyleLayer(identifier: "park-feature-points", source: source)
+            layer.sourceLayerIdentifier = "FWS_HQ_NWRS_TrailPts-33isji"
+//            layer.predicate = NSPredicate(format: "station = 'Kenai NWR'")
+            layer.text = NSExpression(forKeyPath: "type")
+            style.addLayer(layer)
+        }
     }
 
 }
