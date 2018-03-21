9298086b29b80f49c92c101215a242c0f5659c25
Nadia Barbosa <captainbarbosa@users.noreply.github.com>
2018-03-21 13:17:22 -0700

    New example: switch between user tracking modes (#151)
    * Adjust arrow background color & line join
    
    * Rough nonfunctional draft
    
    * An unpleasant experience
    
    * Try to debug stuff
    
    * It's dump-o-clock
    
    * Omg things are happening
    
    * Redo arrow path style logic for Swift example
    
    * Refactor button initializer
    
    * Underscore syntax -> self. syntax
    
    * Remove unused arrowPosition
    
    * Clean up comments and debug code
    
    * Refactor arrow style layout logic
    
    AKA make ObjC more ObjC-eey
    
    * Remove button size constant in favor of using stored property value
    
    * Add pragma marks
    
    * Use dot syntax
    
    * Use only one switch statement for updating arrow and tracking mode
    
    * Move arrow styling logic to UIButton subclass
    
    * Too many modes in the kitchen
    
    * Put arrow styling elements into variables
    
    * Remove unecessary line
    
    * Color stuff again
    
    * Port over Obj-C refactors to Swift
    
    * Add comments
    
    * Don't set initial tracking mode twice
    
    * No withs!
    
    * Style edits
    
    * Color adjustments
    
    * Re-center arrow if rotated
    
    * One more .
    
    * ¯\_(ツ)_/¯
    
    * Hi I'm Nadia and I don't know how line breaks work

--- Examples/ObjectiveC/UserTrackingModesExample.m
+++ Examples/ObjectiveC/UserTrackingModesExample.m
@@ -3,23 +3,180 @@
 
 NSString *const MBXExampleUserTrackingModes = @"UserTrackingModesExample";
 
+#pragma mark - UIButton subclass
+
+// Subclass UIButton to create a custom user tracking mode button
+@interface UserLocationButton : UIButton
+@property (nonatomic) CAShapeLayer *arrow;
+@property (nonatomic) CGFloat buttonSize;
+@end
+
+@implementation UserLocationButton
+
+// Initializer to create the user tracking mode button
+- (instancetype)initWithButtonSize:(CGFloat)buttonSize {
+    if (self = [super init]) {
+        self.frame = CGRectMake(0, 0, buttonSize, buttonSize);
+        self.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.9];
+        self.layer.cornerRadius = 4;
+        self.buttonSize = buttonSize;
+        
+        CAShapeLayer *arrow = [[CAShapeLayer alloc] init];
+        arrow.path = [self arrowPath];
+        arrow.lineWidth = 2;
+        arrow.lineJoin = kCALineJoinRound;
+        arrow.bounds = CGRectMake(0, 0, buttonSize / 2, buttonSize / 2);
+        arrow.position = CGPointMake(buttonSize / 2, buttonSize / 2);
+        arrow.shouldRasterize = YES;
+        arrow.rasterizationScale = [[UIScreen mainScreen] scale];
+        arrow.drawsAsynchronously = YES;
+        
+        self.arrow = arrow;
+        
+        // Update arrow for initial tracking mode
+        [self updateArrowForTrackingMode:MGLUserTrackingModeNone];
+        
+        [self.layer addSublayer:self.arrow];
+    }
+    
+    return self;
+}
+
+// Create a new bezier path to represent the tracking mode arrow,
+// making sure the arrow does not get drawn outside of the
+// frame size of the UIButton.
+- (CGPathRef)arrowPath {
+    UIBezierPath *bezierPath = [[UIBezierPath alloc] init];
+    CGFloat max = self.buttonSize / 2;
+    [bezierPath moveToPoint:CGPointMake(max * 0.5, 0)];
+    [bezierPath addLineToPoint:CGPointMake(max * 0.1, max)];
+    [bezierPath addLineToPoint:CGPointMake(max * 0.5, max * 0.65)];
+    [bezierPath addLineToPoint:CGPointMake(max * 0.9, max)];
+    [bezierPath addLineToPoint:CGPointMake(max * 0.5, 0)];
+    [bezierPath closePath];
+    
+    return bezierPath.CGPath;
+}
+
+// Update the arrow's color and rotation when tracking mode is changed
+-(void)updateArrowForTrackingMode:(MGLUserTrackingMode)mode {
+    UIColor *activePrimaryColor = UIColor.redColor;
+    UIColor *disabledPrimaryColor = UIColor.clearColor;
+    UIColor *disabledSecondaryColor = UIColor.blackColor;
+    CGFloat rotatedArrow = 0.66;
+    
+    switch (mode) {
+        case MGLUserTrackingModeNone:
+            [self updateArrowFillColor:disabledPrimaryColor
+                           strokeColor:disabledSecondaryColor
+                              rotation:0];
+            break;
+        case MGLUserTrackingModeFollow:
+            [self updateArrowFillColor:disabledPrimaryColor
+                           strokeColor:activePrimaryColor
+                              rotation:0];
+            break;
+        case MGLUserTrackingModeFollowWithHeading:
+            [self updateArrowFillColor:activePrimaryColor
+                           strokeColor:activePrimaryColor
+                              rotation:rotatedArrow];
+            break;
+        case MGLUserTrackingModeFollowWithCourse:
+            [self updateArrowFillColor:activePrimaryColor
+                           strokeColor:activePrimaryColor
+                              rotation:0];
+            break;
+    }
+}
+
+-(void)updateArrowFillColor:(UIColor*)fillColor strokeColor:(UIColor*) strokeColor rotation:(CGFloat) rotation {
+    [self.arrow setFillColor:fillColor.CGColor];
+    [self.arrow setStrokeColor:strokeColor.CGColor];
+    [self.arrow setAffineTransform:CGAffineTransformMakeRotation(rotation)];
+    
+    // Re-center the arrow within the button if rotated
+    if (rotation > 0) {
+        self.arrow.position = CGPointMake(self.buttonSize / 2 + 2, self.buttonSize / 2 - 2);
+    }
+    
+    [self layoutIfNeeded];
+}
+
+@end
+
+#pragma mark - ViewController
+
+@interface UserTrackingModesExample () <MGLMapViewDelegate>
+
+@property (nonatomic) MGLMapView *mapView;
+@property (nonatomic) UserLocationButton IBOutlet *button;
+
+@end
+
 @implementation UserTrackingModesExample
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    MGLMapView *mapView = [[MGLMapView alloc] initWithFrame:self.view.bounds styleURL:[MGLStyle darkStyleURL]];
-    mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    self.mapView = [[MGLMapView alloc] initWithFrame:self.view.bounds styleURL:[MGLStyle darkStyleURL]];
+    self.mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    self.mapView.delegate = self;
 
-    mapView.userTrackingMode = MGLUserTrackingModeFollowWithHeading;
+    // The user location annotation takes its color from the map view's tint color
+    self.mapView.tintColor = [UIColor redColor];
+    self.mapView.attributionButton.tintColor = [UIColor lightGrayColor];
 
-    // The user location annotation takes its color from the map view tint.
-    mapView.tintColor = [UIColor redColor];
+    [self.view addSubview:self.mapView];
+    
+    // Create button to allow user to change the tracking mode
+    [self setupLocationButton];
+}
 
-    // You can set the attribution button tint separately.
-    mapView.attributionButton.tintColor = [UIColor lightGrayColor];
+// Update the user tracking mode when the user toggles through the
+// user tracking mode button.
+-(void)locationButtonTapped:(UserLocationButton *)sender {
+    MGLUserTrackingMode mode;
+    
+    switch (self.mapView.userTrackingMode) {
+        case MGLUserTrackingModeNone:
+            mode = MGLUserTrackingModeFollow;
+            break;
+        case MGLUserTrackingModeFollow:
+            mode = MGLUserTrackingModeFollowWithHeading;
+            break;
+        case MGLUserTrackingModeFollowWithHeading:
+            mode = MGLUserTrackingModeFollowWithCourse;
+            break;
+        case MGLUserTrackingModeFollowWithCourse:
+            mode = MGLUserTrackingModeNone;
+            break;
+    }
 
-    [self.view addSubview:mapView];
+    [self.mapView setUserTrackingMode:mode];
+    [sender updateArrowForTrackingMode:mode];
+}
+
+// Button creation and autolayout setup
+-(void)setupLocationButton {
+    self.button = [[UserLocationButton alloc] initWithButtonSize:80];
+    [self.button addTarget:self action:@selector(locationButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
+    self.button.tintColor = self.mapView.tintColor;
+    [self.view addSubview:self.button];
+    
+    // Setup constraints such that the button is placed within
+    // the upper left corner of the view.
+    self.button.translatesAutoresizingMaskIntoConstraints = NO;
+    NSArray *constraints = @[
+      [NSLayoutConstraint constraintWithItem:self.button attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationGreaterThanOrEqual toItem:self.topLayoutGuide attribute:NSLayoutAttributeBottom multiplier:1 constant:10],
+      [NSLayoutConstraint constraintWithItem:self.button attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeading multiplier:1 constant:10],
+      [NSLayoutConstraint constraintWithItem:self.button attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:self.button.frame.size.height],
+      [NSLayoutConstraint constraintWithItem:self.button attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:self.button.frame.size.width]
+        
+    ];
+    
+    [self.view addConstraints:constraints];
 }
 
 @end
+
+
--- Examples/Swift/UserTrackingModesExample.swift
+++ Examples/Swift/UserTrackingModesExample.swift
@@ -13,19 +13,19 @@ class UserTrackingModesExample_Swift: UIViewController, MGLMapViewDelegate {
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         mapView.delegate = self
         
+        // The user location annotation takes its color from the map view's tint color.
         mapView.tintColor = .red
         mapView.attributionButton.tintColor = .lightGray
         
         view.addSubview(mapView)
         
+        // Create button to allow user to change the tracking mode
         setupLocationButton()
     }
     
-    func mapView(_ mapView: MGLMapView, didChange mode: MGLUserTrackingMode, animated: Bool) {
-        button.updateArrow(for: mode)
-    }
-    
-    @IBAction func locationButtonTapped() {
+    // Update the user tracking mode when the user toggles through the
+    // user tracking mode button.
+    @IBAction func locationButtonTapped(sender: UserLocationButton) {
         var mode: MGLUserTrackingMode
         
         switch (mapView.userTrackingMode) {
@@ -44,17 +44,19 @@ class UserTrackingModesExample_Swift: UIViewController, MGLMapViewDelegate {
         }
         
         mapView.userTrackingMode = mode
+        sender.updateArrowForTrackingMode(mode: mode)
     }
     
+    // Button creation and autolayout setup
     func setupLocationButton() {
-        button = UserLocationButton()
+        button = UserLocationButton(buttonSize: 80)
         button.addTarget(self, action: #selector(locationButtonTapped), for: .touchUpInside)
         button.tintColor = mapView.tintColor
         view.addSubview(button)
         
-        // Do some basic auto layout.
+        // Setup constraints such that the button is placed within
+        // the upper left corner of the view.
         button.translatesAutoresizingMaskIntoConstraints = false
-        
         let constraints = [
             NSLayoutConstraint(item: button, attribute: .top, relatedBy: .greaterThanOrEqual, toItem: topLayoutGuide, attribute: .bottom, multiplier: 1, constant: 10),
             NSLayoutConstraint(item: button, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1, constant: 10),
@@ -66,45 +68,47 @@ class UserTrackingModesExample_Swift: UIViewController, MGLMapViewDelegate {
     }
 }
 
+// MARK: - Custom UIButton subclass
+
 class UserLocationButton : UIButton {
-    private let size: CGFloat = 80
     private var arrow: CAShapeLayer?
+    private let buttonSize: CGFloat
     
-    required init() {
-        super.init(frame: CGRect(x: 0, y: 0, width: size, height: size))
+    // Initializer to create the user tracking mode button
+    init(buttonSize: CGFloat) {
+        self.buttonSize = buttonSize
+        super.init(frame: CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize))
+        self.backgroundColor = UIColor.white.withAlphaComponent(0.9)
+        self.layer.cornerRadius = 4
+        
+        let arrow = CAShapeLayer()
+        arrow.path = arrowPath()
+        arrow.lineWidth = 2
+        arrow.lineJoin = kCALineJoinRound
+        arrow.bounds = CGRect(x: 0, y: 0, width: buttonSize / 2, height: buttonSize / 2)
+        arrow.position = CGPoint(x: buttonSize / 2, y: buttonSize / 2)
+        arrow.shouldRasterize = true
+        arrow.rasterizationScale = UIScreen.main.scale
+        arrow.drawsAsynchronously = true
+        
+        self.arrow = arrow
+        
+        // Update arrow for initial tracking mode
+        updateArrowForTrackingMode(mode: .none)
+//
+        layer.addSublayer(self.arrow!)
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func layoutSubviews() {
-        backgroundColor = UIColor.black.withAlphaComponent(0.5)
-        layer.cornerRadius = 4
-        
-        layoutArrow()
-    }
-    
-    private func layoutArrow() {
-        if arrow == nil {
-            let arrow = CAShapeLayer()
-            arrow.path = arrowPath()
-            arrow.bounds = CGRect(x: 0, y: 0, width: size / 2, height: size / 2)
-            arrow.position = CGPoint(x: size / 2, y: size / 2)
-            arrow.shouldRasterize = true
-            arrow.rasterizationScale = UIScreen.main.scale
-            arrow.drawsAsynchronously = true
-            
-            self.arrow = arrow
-            updateArrow(for: .none)
-            layer.addSublayer(self.arrow!)
-        }
-    }
-    
+    // Create a new bezier path to represent the tracking mode arrow,
+    // making sure the arrow does not get drawn outside of the
+    // frame size of the UIButton.
     private func arrowPath() -> CGPath {
-        let max: CGFloat = size / 2
-        
         let bezierPath = UIBezierPath()
+        let max: CGFloat = buttonSize / 2
         bezierPath.move(to: CGPoint(x: max * 0.5, y: 0))
         bezierPath.addLine(to: CGPoint(x: max * 0.1, y: max))
         bezierPath.addLine(to: CGPoint(x: max * 0.5, y: max * 0.65))
@@ -115,25 +119,39 @@ class UserLocationButton : UIButton {
         return bezierPath.cgPath
     }
     
-    func updateArrow(for mode: MGLUserTrackingMode) {
-        var stroke: CGColor
-        switch (mode) {
+    // Update the arrow's color and rotation when tracking mode is changed.
+    func updateArrowForTrackingMode(mode: MGLUserTrackingMode) {
+        let activePrimaryColor = UIColor.red
+        let disabledPrimaryColor = UIColor.clear
+        let disabledSecondaryColor = UIColor.black
+        let rotatedArrow = CGFloat(0.66)
+        
+        switch mode {
         case .none:
-            stroke = UIColor.white.cgColor
+            updateArrow(fillColor: disabledPrimaryColor, strokeColor: disabledSecondaryColor, rotation: 0)
+            break
         case .follow:
-            stroke = tintColor.cgColor
-        case .followWithHeading, .followWithCourse:
-            stroke = UIColor.clear.cgColor
+            updateArrow(fillColor: disabledPrimaryColor, strokeColor: activePrimaryColor, rotation: 0)
+            break
+        case .followWithHeading:
+            updateArrow(fillColor: activePrimaryColor, strokeColor: activePrimaryColor, rotation: rotatedArrow)
+            break
+        case .followWithCourse:
+            updateArrow(fillColor: activePrimaryColor, strokeColor: activePrimaryColor, rotation: 0)
+            break
         }
-        arrow!.strokeColor = stroke
+    }
+    
+    func updateArrow(fillColor: UIColor, strokeColor: UIColor, rotation: CGFloat) {
+        guard let arrow = arrow else { return }
+        arrow.fillColor = fillColor.cgColor
+        arrow.strokeColor = strokeColor.cgColor
+        arrow.setAffineTransform(CGAffineTransform.identity.rotated(by: rotation))
         
-        // Re-center the arrow, based on its current orientation.
-        arrow!.position = (mode == .none || mode == .followWithCourse) ? CGPoint(x: size / 2, y: size / 2) : CGPoint(x: size / 2 + 2, y: size / 2 - 2)
-        
-        arrow!.fillColor = (mode == .none || mode == .follow) ? UIColor.clear.cgColor : tintColor.cgColor
-        
-        let rotation: CGFloat = (mode == .follow || mode == .followWithHeading) ? 0.66 : 0
-        arrow!.setAffineTransform(CGAffineTransform.identity.rotated(by: rotation))
+        // Re-center the arrow within the button if rotated
+        if rotation > 0 {
+            arrow.position = CGPoint(x: buttonSize / 2 + 2, y: buttonSize / 2 - 2)
+        }
         
         layoutIfNeeded()
     }
