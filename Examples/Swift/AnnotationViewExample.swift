adeb2a9d47cca3dca2985bd05c4bca2b4f9ca40d
Jason Wray <jason@kulturny.com>
2018-03-22 13:54:26 -0400

    Animate appearance of view annotation

--- Examples/Swift/AnnotationViewExample.swift
+++ Examples/Swift/AnnotationViewExample.swift
@@ -4,10 +4,12 @@ import Mapbox
 
 // Example view controller
 class AnnotationViewExample_Swift: UIViewController, MGLMapViewDelegate {
+    var mapView: MGLMapView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        let mapView = MGLMapView(frame: view.bounds)
+
+        mapView = MGLMapView(frame: view.bounds)
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         mapView.styleURL = MGLStyle.darkStyleURL()
         mapView.tintColor = .lightGray
@@ -15,14 +17,20 @@ class AnnotationViewExample_Swift: UIViewController, MGLMapViewDelegate {
         mapView.zoomLevel = 2
         mapView.delegate = self
         view.addSubview(mapView)
-        
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        guard mapView.annotations == nil else { return }
+
         // Specify coordinates for our annotations.
         let coordinates = [
             CLLocationCoordinate2D(latitude: 0, longitude: 33),
             CLLocationCoordinate2D(latitude: 0, longitude: 66),
             CLLocationCoordinate2D(latitude: 0, longitude: 99),
         ]
-        
+
         // Fill an array with point annotations and add it to the map.
         var pointAnnotations = [MGLPointAnnotation]()
         for coordinate in coordinates {
@@ -31,38 +39,38 @@ class AnnotationViewExample_Swift: UIViewController, MGLMapViewDelegate {
             point.title = "\(coordinate.latitude), \(coordinate.longitude)"
             pointAnnotations.append(point)
         }
-        
+
         mapView.addAnnotations(pointAnnotations)
     }
-    
+
     // MARK: - MGLMapViewDelegate methods
-    
+
     // This delegate method is where you tell the map to load a view for a specific annotation. To load a static MGLAnnotationImage, you would use `-mapView:imageForAnnotation:`.
     func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? {
         // This example is only concerned with point annotations.
         guard annotation is MGLPointAnnotation else {
             return nil
         }
-        
+
         // Use the point annotation’s longitude value (as a string) as the reuse identifier for its view.
         let reuseIdentifier = "\(annotation.coordinate.longitude)"
-        
+
         // For better performance, always try to reuse existing annotations.
         var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier)
-        
+
         // If there’s no reusable annotation view available, initialize a new one.
         if annotationView == nil {
             annotationView = CustomAnnotationView(reuseIdentifier: reuseIdentifier)
             annotationView!.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
-            
+
             // Set the annotation view’s background color to a value determined by its longitude.
             let hue = CGFloat(annotation.coordinate.longitude) / 100
             annotationView!.backgroundColor = UIColor(hue: hue, saturation: 0.5, brightness: 1, alpha: 1)
         }
-        
+
         return annotationView
     }
-    
+
     func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
         return true
     }
@@ -73,19 +81,28 @@ class AnnotationViewExample_Swift: UIViewController, MGLMapViewDelegate {
 class CustomAnnotationView: MGLAnnotationView {
     override func layoutSubviews() {
         super.layoutSubviews()
-        
+
         // Force the annotation view to maintain a constant size when the map is tilted.
         scalesWithViewingDistance = false
-        
+
         // Use CALayer’s corner radius to turn this view into a circle.
         layer.cornerRadius = frame.width / 2
         layer.borderWidth = 2
         layer.borderColor = UIColor.white.cgColor
     }
-    
+
+    override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+
+        transform = CGAffineTransform(scaleX: 0, y: 0)
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: UIViewAnimationOptions(), animations: {
+            self.transform = .identity
+        })
+    }
+
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        
+
         // Animate the border width in/out, creating an iris effect.
         let animation = CABasicAnimation(keyPath: "borderWidth")
         animation.duration = 0.1
