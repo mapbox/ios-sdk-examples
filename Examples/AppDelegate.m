d1d6c1cfbf10dec11eace2c3a7ab124123069d1b
jmkiley <jordan.kiley@mapbox.com>
2018-04-12 12:55:07 -0700

    Add popup for access token

--- Examples/AppDelegate.m
+++ Examples/AppDelegate.m
@@ -7,16 +7,32 @@
 //
 
 #import "AppDelegate.h"
+@import Mapbox;
 
 @interface AppDelegate ()
 
 @end
 @implementation AppDelegate
+NSString * const MBXMapboxAccessTokenDefaultsKey = @"MBXMapboxAccessToken";
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Speed-up Core Animation-based animations in testing scenarios.
     if ([[NSProcessInfo processInfo].arguments containsObject:@"useFastAnimations"]) {
         self.window.layer.speed = 100;
+    }
+
+    if ( ! [MGLAccountManager accessToken]) {
+        NSString *accessToken = [[NSProcessInfo processInfo] environment][@"MAPBOX_ACCESS_TOKEN"];
+        if (accessToken) {
+            // Store to preferences so that we can launch the app later on without having to specify
+            // token.
+            [[NSUserDefaults standardUserDefaults] setObject:accessToken forKey:MBXMapboxAccessTokenDefaultsKey];
+        } else {
+            // Try to retrieve from preferences, maybe we've stored them there previously and can reuse
+            // the token.
+            accessToken = [[NSUserDefaults standardUserDefaults] objectForKey:MBXMapboxAccessTokenDefaultsKey];
+        }
+        [MGLAccountManager setAccessToken:accessToken];
     }
 
     return YES;
--- Podfile
+++ Podfile
@@ -9,12 +9,7 @@ end
 
 target 'DocsCode' do
   platform :ios, '9.0'
-  # Navigation examples will need to move to
-  # https://github.com/mapbox/navigation-ios-examples
-  # in order to avoid version conflicts with the
-  # Maps SDK in the examples target
-  # pod 'MapboxNavigation', '~> 0.11'
-  pod 'Mapbox-iOS-SDK', podspec: 'https://raw.githubusercontent.com/mapbox/mapbox-gl-native/ios-v4.0.0-beta.3/platform/ios/Mapbox-iOS-SDK.podspec'
+  #pod 'MapboxNavigation', '~> 0.11'
 end
 
 target 'ExamplesTests' do
