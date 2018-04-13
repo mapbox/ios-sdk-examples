a9d1e6efa093479cfbc5cc54edb98ec8ac321a0f
Nadia Barbosa <captainbarbosa@users.noreply.github.com>
2018-04-13 08:42:22 -0700

    Upgrade tutorial code, remove nav tutorial from DocsCode target

--- DocsCode/DDSCircleLayerTutorial/DDSCircleLayerTutorialViewController.m
+++ DocsCode/DDSCircleLayerTutorial/DDSCircleLayerTutorialViewController.m
@@ -19,8 +19,8 @@
     mapView.tintColor = [UIColor darkGrayColor];
     
     // Set the map’s center coordinate and zoom level.
-    mapView.centerCoordinate = CLLocationCoordinate2DMake(38.897, -77.039);
-    mapView.zoomLevel = 10.5;
+    mapView.centerCoordinate = CLLocationCoordinate2DMake(44.971, -93.261);
+    mapView.zoomLevel = 10;
     
     [self.view addSubview:mapView];
     mapView.delegate = self;
@@ -32,7 +32,7 @@
     // #-code-snippet: dds-circle add-vector-source-objc
     // "mapbox://examples.2uf7qges" is the map ID referencing a tileset
     // created from the GeoJSON data uploaded earlier.
-    MGLSource *source = [[MGLVectorSource alloc] initWithIdentifier:@"trees" configurationURL:[NSURL URLWithString:@"mapbox://examples.2uf7qges"]];
+    MGLSource *source = [[MGLVectorTileSource alloc] initWithIdentifier:@"historical-places" configurationURL:[NSURL URLWithString:@"mapbox://cmcg.71x8omg1"]];
     
     [mapView.style addSource:source];
     // #-end-code-snippet: dds-circle add-vector-source-objc
@@ -41,26 +41,23 @@
     MGLCircleStyleLayer *layer = [[MGLCircleStyleLayer alloc] initWithIdentifier:@"tree-style" source:source];
     
     // The source name from the source's TileJSON metadata: mapbox.com/api-documentation/#retrieve-tilejson-metadata
-    layer.sourceLayerIdentifier = @"yoshino-trees-a0puw5";
+    layer.sourceLayerIdentifier = @"HPC_landmarks-39z2b05";
     // #-end-code-snippet: dds-circle add-circle-layer-objc
     
     // #-code-snippet: dds-circle add-stops-dictionary-objc
     NSDictionary *stops = @{
-        @0: [NSExpression expressionForConstantValue:[UIColor colorWithRed:1.00 green:0.72 blue:0.85 alpha:1.0]],
-        @2: [NSExpression expressionForConstantValue:[UIColor colorWithRed:0.69 green:0.48 blue:0.73 alpha:1.0]],
-        @4: [NSExpression expressionForConstantValue:[UIColor colorWithRed:0.61 green:0.31 blue:0.47 alpha:1.0]],
-        @7: [NSExpression expressionForConstantValue:[UIColor colorWithRed:0.43 green:0.20 blue:0.38 alpha:1.0]],
-        @16: [NSExpression expressionForConstantValue:[UIColor colorWithRed:0.33 green:0.17 blue:0.25 alpha:1.0]]
+        @0: [UIColor colorWithRed:1.00 green:0.72 blue:0.85 alpha:1.0],
+        @2: [UIColor colorWithRed:0.69 green:0.48 blue:0.73 alpha:1.0],
+        @4: [UIColor colorWithRed:0.61 green:0.31 blue:0.47 alpha:1.0],
+        @7: [UIColor colorWithRed:0.43 green:0.20 blue:0.38 alpha:1.0],
+        @16: [UIColor colorWithRed:0.33 green:0.17 blue:0.25 alpha:1.0]
     };
     // #-end-code-snippet: dds-circle add-stops-dictionary-objc
     
     // #-code-snippet: dds-circle add-style-layer-objc
     // Style the circle layer color based on the above categorical stops.
-    layer.circleColor = [MGLStyleValue valueWithInterpolationMode:MGLInterpolationModeInterval
-                                                      sourceStops:stops
-                                                    attributeName:@"AGE"
-                                                          options:nil];
-    
+    layer.circleColor = [NSExpression expressionWithFormat:@"mgl_step:from:stops:(AGE, %@, %@)", [UIColor colorWithRed:1.0 green:0.72 blue:0.85 alpha:1.0], stops];
+
     layer.circleRadius = [NSExpression expressionForConstantValue:@3];
     
     [mapView.style addLayer:layer];
--- Podfile
+++ Podfile
@@ -7,7 +7,12 @@ end
 
 target 'DocsCode' do
   platform :ios, '9.0'
-#  pod 'MapboxNavigation', '~> 0.11'
+  # Navigation examples will need to move to
+  # https://github.com/mapbox/navigation-ios-examples
+  # in order to avoid version conflicts with the
+  # Maps SDK in the examples target
+  # pod 'MapboxNavigation', '~> 0.11'
+  pod 'Mapbox-iOS-SDK', podspec: 'https://raw.githubusercontent.com/mapbox/mapbox-gl-native/ios-v4.0.0-beta.3/platform/ios/Mapbox-iOS-SDK.podspec'
 end
 
 target 'ExamplesTests' do
