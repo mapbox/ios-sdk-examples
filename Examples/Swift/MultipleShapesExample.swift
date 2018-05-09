3364c39bbcfb785ee13f722057accf330d451354
Nadia Barbosa <captainbarbosa@users.noreply.github.com>
2018-05-09 10:41:39 -0700

    Try to cast string to UIColor

--- Examples/Swift/MultipleShapesExample.swift
+++ Examples/Swift/MultipleShapesExample.swift
@@ -14,7 +14,7 @@ class MultipleShapesExample_Swift: UIViewController, MGLMapViewDelegate {
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         mapView.tintColor = .darkGray
         
-        mapView.setCenter(CLLocationCoordinate2D(latitude:38.897435, longitude: -77.039679), zoomLevel: 12, animated: false)
+        mapView.setCenter(CLLocationCoordinate2D(latitude:36.932804264700003, longitude: -76.296154697199995), zoomLevel: 12, animated: false)
         
         mapView.delegate = self
         view.addSubview(mapView)
@@ -38,7 +38,7 @@ class MultipleShapesExample_Swift: UIViewController, MGLMapViewDelegate {
         guard let style = self.mapView.style else { return }
         
         // Use [MGLShape shapeWithData:encoding:error:] to create a MGLShapeCollectionFeature from GeoJSON data.
-        let feature = try! MGLShape(data: data, encoding: String.Encoding.utf8.rawValue) as! MGLShapeCollectionFeature
+        let feature = try! MGLShape(data: data, encoding: String.Encoding.utf8.rawValue) as! MGLPolylineFeature
         
         // Create source and add it to the map style.
         let source = MGLShapeSource(identifier: "line-source", shape: feature, options: nil)
@@ -47,7 +47,31 @@ class MultipleShapesExample_Swift: UIViewController, MGLMapViewDelegate {
         // Create line style layer.
         let lineLayer = MGLLineStyleLayer(identifier: "line", source: source)
         
-        lineLayer.lineColor = NSExpression(forKeyPath: "color")
+        // This is what the properties look like
+        // for the GeoJSON
+        
+//        "properties":{
+//            "lineStyle":{
+//                "color":"#00ffff",
+//                "opacity":1,
+//                "weight":1
+//            },
+//            "polyStyle":{
+//                "fillOpacity":0,
+//                "weight":1,
+//                "fillColor":"#00ffff",
+//                "opacity":1,
+//                "color":"#00ffff"
+//            },
+//            "pointStyle":{
+//                "radius":7,
+//                "weight":0.5,
+//                "opacity":1,
+//                "color":"#00ffff"
+//            }
+//        }
+//
+        lineLayer.lineColor = NSExpression(format: "CAST(lineStyle.color, 'UIColor')")
         
         lineLayer.lineWidth = NSExpression(forConstantValue: 2)
         
