5f4d2dbcebbb5073ebd0abb727995e07b8f05f5f
Lloyd Sheng <i@lloydsheng.com>
2018-05-11 14:02:49 +0800

    Example list should be better organized and more friendly (#172)
    * Reorganize examples with categories and improve iPad user experience.
    
    * Correction of annotation examples category name

--- Examples/Examples.m
+++ Examples/Examples.m
@@ -10,63 +10,127 @@
 
 @implementation Examples
 
-+ (NSArray *)list {
-    
++ (NSArray *)groups {
     NSArray *initialList = [[NSMutableArray alloc] initWithArray:@[
-        MBXExampleAnimatedLine,
-        MBXExampleAnnotationView,
-        MBXExampleAnnotationViewsAndImagesExample,
-        MBXExampleBlockingGesturesDelegate,
-        MBXExampleBuildingLight,
-        MBXExampleBuildings,
-        MBXExampleCalloutDelegateUsage,
-        MBXExampleCameraAnimation,
-        MBXExampleCameraFlyTo,
-        MBXExampleClustering,
-        MBXExampleCustomAnnotationModel,
-        MBXExampleCustomCalloutView,
-        MBXExampleDDSCircleLayer,
-        MBXExampleDefaultAnnotation,
-        MBXExampleDefaultStyles,
-        MBXExampleDraggableAnnotationView,
-        MBXExampleFeatureSelection,
-        MBXExampleImageAnnotation,
-        MBXExampleImageSource,
-        MBXExampleLineAnnotationGeoJSON,
-        MBXExampleLineStyleLayer,
-        MBXExampleLiveData,
-        MBXExampleMultipleImages,
-        MBXExampleMultipleShapes,
-        MBXExampleOfflinePack,
-        MBXExamplePointConversion,
-        MBXExamplePointHotspot,
-        MBXExamplePolygonAnnotation,
-        MBXExamplePolygonPattern,
-        MBXExampleRasterImagery,
-        MBXExampleSatelliteStyle,
-        MBXExampleSelectFeature,
-        MBXExampleShowHideLayer,
-        MBXExampleSimpleMapView,
-        MBXExampleStaticSnapshot,
-        MBXExampleStudioClassicStyle,
-        MBXExampleStudioStyle,
-        MBXExampleSwitchStyles,
-        MBXExampleThirdPartyVectorStyle,
-        MBXExampleUserLocationAnnotation,
-        MBXExampleUserTrackingModes,
-        MBXExampleWebAPIData,
+        @{
+            @"title": @"Getting started",
+            @"examples":@[
+                    @{@"className": MBXExampleCameraAnimation, @"title": @"Camera animation"},
+                    @{@"className": MBXExampleStudioStyle, @"title": @"Apply a style designed in Mapbox Studio"},
+                    @{@"className": MBXExampleDefaultAnnotation, @"title": @"Mark a place on the map with an annotation"},
+                    @{@"className": MBXExampleSimpleMapView, @"title": @"Simple map view"},
+            ]
+        },
+        @{
+            @"title": @"Map styles",
+            @"examples":@[
+                    @{@"className": MBXExampleDefaultStyles, @"title": @"Default styles"},
+                    @{@"className": MBXExampleStudioClassicStyle, @"title": @"Apply a style designed in Mapbox Studio Classic"},
+                    @{@"className": MBXExampleSatelliteStyle, @"title": @"Satellite styles"},
+                    @{@"className": MBXExampleThirdPartyVectorStyle, @"title": @"Use third-party vector tiles"},
+                    @{@"className": MBXExampleSwitchStyles, @"title": @"Switch between map styles"},
+                    @{@"className": MBXExampleCameraFlyTo, @"title": @"Camera fly to"},
+            ]
+        },
+        @{
+            @"title": @"Annotations",
+            @"examples":@[
+                    @{@"className": MBXExampleCustomAnnotationModel, @"title": @"Annotation models"},
+                    @{@"className": MBXExampleAnnotationViewsAndImagesExample, @"title": @"Add annotation views and images"},
+                    @{@"className": MBXExampleAnnotationView, @"title": @"Annotation views"},
+                    @{@"className": MBXExampleCalloutDelegateUsage, @"title": @"Callout delegate usage"},
+                    @{@"className": MBXExampleCustomCalloutView, @"title": @"Display custom views as callouts"},
+                    @{@"className": MBXExampleImageAnnotation, @"title": @"Mark a place on the map with an image"},
+                    @{@"className": MBXExampleWebAPIData, @"title": @"Dynamically style interactive points"},
+                    @{@"className": MBXExampleUserLocationAnnotation, @"title": @"Customize the user location annotation"},
+                    ]
+        },
+        @{
+            @"title": @"Markers and callouts",
+            @"examples":@[
+                    @{@"className": MBXExamplePolygonPattern, @"title": @"Add a pattern to a polygon"},
+                    @{@"className": MBXExampleLineAnnotationGeoJSON, @"title": @"Add a line annotation from GeoJSON"},
+                    @{@"className": MBXExamplePolygonAnnotation, @"title": @"Add a polygon annotation"},
+                    @{@"className": MBXExampleLineStyleLayer, @"title": @"Add a line style layer from GeoJSON"},
+                    @{@"className": MBXExampleAnimatedLine, @"title": @"Animate a line"},
+                    ]
+        },
+        @{
+            @"title": @"User interaction",
+            @"examples":@[
+                    @{@"className": MBXExampleBlockingGesturesDelegate, @"title": @"Restrict map panning to an area"},
+                    @{@"className": MBXExampleDraggableAnnotationView, @"title": @"Draggable annotation views"},
+                    @{@"className": MBXExamplePointConversion, @"title": @"Point conversion"},
+                    @{@"className": MBXExampleShowHideLayer, @"title": @"Show and hide a layer"},
+                    @{@"className": MBXExampleFeatureSelection, @"title": @"Select a feature within a layer"},
+                    @{@"className": MBXExampleSelectFeature, @"title": @"Select a park feature"},
+                    @{@"className": MBXExampleUserTrackingModes, @"title": @"Switch between user tracking modes"},
+                    ]
+        },
+        @{
+            @"title": @"Dynamic styling",
+            @"examples":@[
+                    @{@"className": MBXExampleClustering, @"title": @"Cluster point data"},
+                    @{@"className": MBXExampleDDSCircleLayer, @"title": @"Data-driven circles"},
+                    @{@"className": MBXExampleImageSource, @"title": @"Add an image"},
+                    @{@"className": MBXExampleLiveData, @"title": @"Add live data"},
+                    @{@"className": MBXExampleMultipleShapes, @"title": @"Add multiple shapes from a single shape source"},
+                    @{@"className": MBXExampleMultipleImages, @"title": @"Add multiple images"},
+                    @{@"className": MBXExampleRasterImagery, @"title": @"Add raster imagery"},
+                    @{@"className": MBXExamplePointHotspot, @"title": @"Add point hotspot"},
+                    ]
+        },
+        @{
+            @"title": @"3D",
+            @"examples":@[
+                    @{@"className": MBXExampleBuildings, @"title": @"Display 3D buildings"},
+                    @{@"className": MBXExampleBuildingLight, @"title": @"Adjust lighting of 3D buildings"},
+                    ]
+        },
+        @{
+            @"title": @"Offline",
+            @"examples":@[
+                    @{@"className": MBXExampleStaticSnapshot, @"title": @"Create a static map snapshot"},
+                    @{@"className": MBXExampleOfflinePack, @"title": @"Download an offline map"},
+                    ]
+        }
     ]];
 
-    NSMutableArray *meh = [[NSMutableArray alloc] init];
+    NSMutableArray *objcCategories = [[NSMutableArray alloc] init];
+    NSMutableArray *swifCategories = [[NSMutableArray alloc] init];
     
-    [initialList enumerateObjectsUsingBlock:^(NSString *objcName, NSUInteger index, BOOL *stop) {
-        NSString *swiftName = [NSString stringWithFormat:@"%@_Swift", objcName];
+    [initialList enumerateObjectsUsingBlock:^(NSDictionary *category, NSUInteger index, BOOL *stop) {
+        NSMutableArray *objcExamples = [[NSMutableArray alloc] init];
+        NSMutableArray *swiftExamples = [[NSMutableArray alloc] init];
         
-        [meh insertObject:swiftName atIndex:index*2];
-        [meh insertObject:objcName atIndex:index*2];
+        NSArray *examples = category[@"examples"];
+        [examples  enumerateObjectsUsingBlock:^(NSDictionary *example, NSUInteger index, BOOL *stop) {
+            NSString *objcName = example[@"className"];
+            NSString *swiftName = [NSString stringWithFormat:@"%@_Swift", objcName];
+            NSString *title = example[@"title"] ? example[@"title"] : example[@"className"];
+            
+            [objcExamples addObject:@{@"className": objcName,
+                                      @"title": title
+                                      }];
+            [swiftExamples addObject:@{@"className": swiftName,
+                                       @"title": title
+                                       }];
+        }];
+        
+        [objcCategories addObject:@{
+                                    @"title": category[@"title"],
+                                    @"examples": objcExamples
+                                    }];
+        [swifCategories addObject:@{
+                                    @"title": category[@"title"],
+                                    @"examples": objcExamples
+                                    }];
     }];
     
-    return [meh copy];
+    return @[
+             @{@"title": @"Swift", @"categories": swifCategories},
+             @{@"title": @"ObjC", @"categories": objcCategories}
+             ];
 }
 
 @end
