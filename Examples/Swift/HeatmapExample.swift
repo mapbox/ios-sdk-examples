75b4f851474574f17c64e2dbe996a0444a5aced0
Nadia Barbosa <nadiabarbosa@me.com>
2018-07-31 14:58:51 -0700

    : :arrow_left: :arrow_left: :arrow_left:

--- Examples/Swift/HeatmapExample.swift
+++ Examples/Swift/HeatmapExample.swift
@@ -29,8 +29,8 @@ class HeatmapExample: UIViewController, MGLMapViewDelegate {
                                 0.0: .clear,
                                0.01: .white,
                                0.15: UIColor(red: 0.19, green: 0.30, blue: 0.80, alpha: 1.0),
-                               0.5: UIColor(red: 0.73, green: 0.23, blue: 0.25, alpha: 1.0),
-                               1: .yellow
+                                0.5: UIColor(red: 0.73, green: 0.23, blue: 0.25, alpha: 1.0),
+                                  1: .yellow
         ]
         heatmapLayer.heatmapColor = NSExpression(format: "mgl_interpolate:withCurveType:parameters:stops:($heatmapDensity, 'linear', nil, %@)", colorDictionary)
         
@@ -55,9 +55,9 @@ class HeatmapExample: UIViewController, MGLMapViewDelegate {
         let circleLayer = MGLCircleStyleLayer(identifier: "circle-layer", source: source)
         
         let magnitudeDictionary: [NSNumber: UIColor] = [0: .white,
-                                                          0.5: .yellow,
-                                                          2.5: UIColor(red: 0.73, green: 0.23, blue: 0.25, alpha: 1.0),
-                                                          5: UIColor(red: 0.19, green: 0.30, blue: 0.80, alpha: 1.0)
+                                                      0.5: .yellow,
+                                                      2.5: UIColor(red: 0.73, green: 0.23, blue: 0.25, alpha: 1.0),
+                                                        5: UIColor(red: 0.19, green: 0.30, blue: 0.80, alpha: 1.0)
         ]
         circleLayer.circleColor = NSExpression(format: "mgl_interpolate:withCurveType:parameters:stops:(mag, 'linear', nil, %@)", magnitudeDictionary)
         
