6b265de2a7ba78b228cbbbe492e0993d844ceba3
Josh Erb <josh.erb@mapbox.com>
2018-08-08 13:24:25 -0400

    update shapes and stops

--- Examples/Swift/ClusteringWithImagesExample.swift
+++ Examples/Swift/ClusteringWithImagesExample.swift
@@ -5,7 +5,7 @@ import Mapbox
 class ClusteringWithImagesExample_Swift: UIViewController, MGLMapViewDelegate {
     
     var mapView: MGLMapView!
-    var icon: UIImage!
+    var icon = UIImage(named: "squircle")!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,8 +15,6 @@ class ClusteringWithImagesExample_Swift: UIViewController, MGLMapViewDelegate {
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         mapView.delegate = self
         view.addSubview(mapView)
-        
-        icon = UIImage(named: "circle")
     }
 
     func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
@@ -35,20 +33,21 @@ class ClusteringWithImagesExample_Swift: UIViewController, MGLMapViewDelegate {
         
         
         // Style clusters
+        style.setImage(UIImage(named: "squircle")!, forName: "squircle")
         style.setImage(UIImage(named: "circle")!, forName: "circle")
         style.setImage(UIImage(named: "rectangle")!, forName: "rectangle")
-        style.setImage(UIImage(named: "cloud")!, forName: "cloud")
+        style.setImage(UIImage(named: "star")!, forName: "star")
         style.setImage(UIImage(named: "oval")!, forName: "oval")
         
         let stops = [
             10:  NSExpression(forConstantValue: "circle"),
             25:  NSExpression(forConstantValue: "rectangle"),
-            75: NSExpression(forConstantValue: "cloud"),
+            75: NSExpression(forConstantValue: "star"),
             150: NSExpression(forConstantValue: "oval")
         ]
         
-        let defaultCircle = NSExpression(forConstantValue: "circle")
-        numbersLayer.iconImageName = NSExpression(format: "mgl_step:from:stops:(point_count, %@, %@)", defaultCircle, stops)
+        let defaultShape = NSExpression(forConstantValue: "squircle")
+        numbersLayer.iconImageName = NSExpression(format: "mgl_step:from:stops:(point_count, %@, %@)", defaultShape, stops)
         numbersLayer.text = NSExpression(format: "CAST(point_count, 'NSString')")
         
         style.addLayer(numbersLayer)
