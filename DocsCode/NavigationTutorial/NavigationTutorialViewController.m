8b54cd4db410db6d66925152234fe7766f0e4e6e
Minh Nguyễn <mxn@1ec5.org>
2018-09-17 22:02:56 -0700

    Navigation SDK v0.21.0

--- DocsCode/NavigationTutorial/NavigationTutorialViewController.m
+++ DocsCode/NavigationTutorial/NavigationTutorialViewController.m
@@ -134,7 +134,7 @@
 
 // Present the navigation view controller when the callout is selected
 - (void)mapView:(MGLMapView *)mapView tapOnCalloutForAnnotation:(id<MGLAnnotation>)annotation {
-    MBNavigationViewController *navigationViewController = [[MBNavigationViewController alloc] initWithRoute:self.directionsRoute directions:[MBDirections sharedDirections] styles:nil locationManager:nil voiceController:nil];
+    MBNavigationViewController *navigationViewController = [[MBNavigationViewController alloc] initWithRoute:self.directionsRoute directions:[MBDirections sharedDirections] styles:nil routeController:nil locationManager:nil voiceController:nil eventsManager:nil];
     [self presentViewController:navigationViewController animated:YES completion:nil];
 }
 // #-end-code-snippet: navigation callout-functions-objc
