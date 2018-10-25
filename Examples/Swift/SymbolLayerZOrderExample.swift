6d89cb1d3dad2684661d32344c437a5c26def3f0
Sam Fader <sam.fader@mapbox.com>
2018-10-25 15:01:15 -0700

    add toggle to switch between z order type

--- Examples/Swift/SymbolLayerZOrderExample.swift
+++ Examples/Swift/SymbolLayerZOrderExample.swift
@@ -14,6 +14,7 @@ import Mapbox
 class SymbolLayerZOrderExample_Swift: UIViewController, MGLMapViewDelegate {
 
 var mapView: MGLMapView!
+var symbolLayer: MGLSymbolStyleLayer?
 
 override func viewDidLoad() {
     super.viewDidLoad()
@@ -58,8 +59,44 @@ func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
         layer.iconImageName = NSExpression(format: "FUNCTION(%@, 'valueForKeyPath:', id)", icons)
     
         layer.iconAllowsOverlap = NSExpression(forConstantValue: true)
-        layer.iconIgnoresPlacement = NSExpression(forConstantValue: true)
         layer.symbolZOrder = NSExpression(forConstantValue: "source")
         style.addLayer(layer)
+    
+        self.symbolLayer = layer
+    
+        // Create a UISegmentedControl to toggle between map styles
+        let styleToggle = UISegmentedControl(items: ["viewport-y", "source"])
+        styleToggle.translatesAutoresizingMaskIntoConstraints = false
+        styleToggle.tintColor = UIColor(red: 0.976, green: 0.843, blue: 0.831, alpha: 1)
+        styleToggle.backgroundColor = UIColor(red: 0.973, green: 0.329, blue: 0.294, alpha: 1)
+        styleToggle.layer.cornerRadius = 4
+        styleToggle.clipsToBounds = true
+        styleToggle.selectedSegmentIndex = 1
+        view.insertSubview(styleToggle, aboveSubview: mapView)
+        styleToggle.addTarget(self, action: #selector(toggleLayer(sender:)), for: .valueChanged)
+    
+        // Configure autolayout constraints for the UISegmentedControl to align
+        // at the bottom of the map view and above the Mapbox logo and attribution
+        NSLayoutConstraint.activate([NSLayoutConstraint(item: styleToggle, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: mapView, attribute: NSLayoutAttribute.centerX, multiplier: 1.0, constant: 0.0)])
+        NSLayoutConstraint.activate([NSLayoutConstraint(item: styleToggle, attribute: .bottom, relatedBy: .equal, toItem: mapView.logoView, attribute: .top, multiplier: 1, constant: -20)])
+    }
+    // Change the map style based on the selected index of the UISegmentedControl
+    @objc func toggleLayer(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            useSource()
+        case 1:
+            useViewportY()
+        default:
+            useSource()
+        }
+    }
+    
+    func useSource() {
+        self.symbolLayer?.symbolZOrder = NSExpression(forConstantValue: "source")
+    }
+    
+    func useViewportY() {
+        self.symbolLayer?.symbolZOrder = NSExpression(forConstantValue: "viewport-y")
     }
 }
