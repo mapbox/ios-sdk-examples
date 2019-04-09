b2683bde281a951e05860178f9fc1faeb36c93ec
Fabian Guerra Soto <fabian.guerra@mapbox.com>
2019-04-09 11:37:59 -0700

    [ios] Add example for label placement. (#286)
    Added test case for text label placement feature.

--- Examples/Examples.h
+++ Examples/Examples.h
@@ -38,6 +38,7 @@ extern NSString *const MBXExampleFeatureSelection;
 extern NSString *const MBXExampleHeatmap;
 extern NSString *const MBXExampleImageAnnotation;
 extern NSString *const MBXExampleImageSource;
+extern NSString *const MBXExampleLabelPlacement;
 extern NSString *const MBXExampleLineAnnotationGeoJSON;
 extern NSString *const MBXExampleLineStyleLayer;
 extern NSString *const MBXExampleLiveData;
