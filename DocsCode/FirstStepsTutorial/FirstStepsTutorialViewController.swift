6fe265896ba584899f98c2dd06e28e3de7a52c60
Jason Wray <jason@kulturny.com>
2019-07-22 15:22:55 -0700

    Update DocsCode target to fix Swift/deprecation warnings

--- DocsCode/FirstStepsTutorial/FirstStepsTutorialViewController.swift
+++ DocsCode/FirstStepsTutorial/FirstStepsTutorialViewController.swift
@@ -46,7 +46,7 @@ class FirstStepsTutorialViewController: UIViewController, MGLMapViewDelegate {
 
     // #-code-snippet: first-steps-ios-sdk fly-to-annotation-swift
     func mapView(_ mapView: MGLMapView, didSelect annotation: MGLAnnotation) {
-        let camera = MGLMapCamera(lookingAtCenter: annotation.coordinate, fromDistance: 4500, pitch: 15, heading: 180)
+        let camera = MGLMapCamera(lookingAtCenter: annotation.coordinate, altitude: 4500, pitch: 15, heading: 180)
         mapView.fly(to: camera, withDuration: 4,
                     peakAltitude: 3000, completionHandler: nil)
     }
