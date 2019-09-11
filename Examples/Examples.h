95a2b8510784356568549d21bead9820dd8ac97e
Nadia Barbosa <nadiabarbosa@me.com>
2019-09-11 12:01:50 -0700

    ✂️ AnnotationViewsAndImagesExample
    Does not spark joy

--- Examples/Examples.h
+++ Examples/Examples.h
@@ -19,7 +19,6 @@
 
 extern NSString *const MBXExampleAnimatedLine;
 extern NSString *const MBXExampleAnnotationView;
-extern NSString *const MBXExampleAnnotationViewsAndImagesExample;
 extern NSString *const MBXExampleBlockingGesturesDelegate;
 extern NSString *const MBXExampleBuildingLight;
 extern NSString *const MBXExampleBuildings;
