51b95004f468dee063b2f4c20c7d7b26f5413fc3
Nadia Barbosa <nadiabarbosa@me.com>
2019-09-17 17:41:28 -0700

    ✂️SelectFeatureExample

--- Examples/Examples.m
+++ Examples/Examples.m
@@ -60,7 +60,6 @@
                     @{@"className": MBXExamplePointConversion, @"title": @"Point conversion"},
                     @{@"className": MBXExampleShowHideLayer, @"title": @"Show and hide a layer"},
                     @{@"className": MBXExampleFeatureSelection, @"title": @"Select a feature within a layer"},
-                    @{@"className": MBXExampleSelectFeature, @"title": @"Select a water feature"},
                     @{@"className": MBXExampleUserTrackingModes, @"title": @"Switch between user tracking modes"},
             ]
         },
