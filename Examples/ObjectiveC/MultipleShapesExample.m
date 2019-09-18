cfedba5e3819a65b7d043eddc702be03dd3069fd
Nadia Barbosa <nadiabarbosa@me.com>
2019-09-17 17:36:20 -0700

    🧹 Clean up MultipleShapesExample
    Now shows three shapes (point, line, polygon)

--- Examples/ObjectiveC/MultipleShapesExample.m
+++ Examples/ObjectiveC/MultipleShapesExample.m
@@ -7,15 +7,20 @@ NSString *const MBXExampleMultipleShapes = @"MultipleShapesExample";
 @property (nonatomic) MGLMapView *mapView;
 @end
 
+@interface MGLStyle (LayerExtensions)
+- (void)addLinesFrom:(MGLShapeSource *)source;
+- (void)addPointsFrom:(MGLShapeSource *)source;
+- (void)addPolygonsFrom:(MGLShapeSource *)source;
+@end
+
 @implementation MultipleShapesExample
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+
+    // Configure a map view centered on Washington, D.C.
     self.mapView = [[MGLMapView alloc] initWithFrame:self.view.bounds styleURL:[MGLStyle lightStyleURL]];
     self.mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    self.mapView.tintColor = [UIColor darkGrayColor];
-    
     [self.mapView setCenterCoordinate:CLLocationCoordinate2DMake(38.897435, -77.039679) zoomLevel:12 animated:NO];
     self.mapView.delegate = self;
     
@@ -23,45 +28,77 @@ NSString *const MBXExampleMultipleShapes = @"MultipleShapesExample";
 }
 
 - (void)mapView:(MGLMapView *)mapView didFinishLoadingStyle:(MGLStyle *)style {
-    
-    // Parse the GeoJSON data.
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        NSString *path = [[NSBundle mainBundle] pathForResource:@"metro-line" ofType:@"geojson"];
-        
-        NSData *data = [NSData dataWithContentsOfFile:path];
-        
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [self drawShapeCollection:data];
-        });
-    });
+    // If this data was coming from an external server, we would want to check if NSURL was nil,
+    // and perform proper error handling for a web request/response.
+    NSURL *url = [NSURL fileURLWithPath:[NSBundle.mainBundle pathForResource:@"metro-line" ofType:@"geojson"]];
+
+    // Create a shape source and register it with the map style.
+    MGLShapeSource *source = [[MGLShapeSource alloc] initWithIdentifier:@"metro-line" URL:url options:nil];
+    [style addSource:source];
+
+    // Add different line, point, and polygon shapes to the map style.
+    [style addLinesFrom:source];
+    [style addPointsFrom:source];
+    [style addPolygonsFrom:source];
 }
 
-- (void)drawShapeCollection:(NSData *)data {
-    
-    // Use [MGLShape shapeWithData:encoding:error:] to create a MGLShapeCollectionFeature from GeoJSON data.
-    MGLShape *feature = [MGLShape shapeWithData:data encoding:NSUTF8StringEncoding error:NULL];
-    
-    // Create source and add it to the map style.
-    MGLShapeSource *source = [[MGLShapeSource alloc] initWithIdentifier:@"transit" shape:feature options:nil];
-    [self.mapView.style addSource:source];
-    
-    // Create station style layer.
-    MGLCircleStyleLayer *circleLayer = [[MGLCircleStyleLayer alloc] initWithIdentifier:@"stations" source:source];
-    circleLayer.predicate = [NSPredicate predicateWithFormat:@"TYPE = 'Station'"];
-    circleLayer.circleColor = [NSExpression expressionForConstantValue:[UIColor redColor]];
-    circleLayer.circleRadius = [NSExpression expressionForConstantValue:@6];
-    circleLayer.circleStrokeWidth = [NSExpression expressionForConstantValue:@2];
-    circleLayer.circleStrokeColor = [NSExpression expressionForConstantValue:[UIColor blackColor]];
-    
-    // Create line style layer.
+@end
+
+@implementation MGLStyle (LayerExtensions)
+
+- (void)addLinesFrom:(MGLShapeSource *)source {
+    /**
+     Configure a line style layer to represent a rail line, filtering out all data from the
+     source that is not of `Rail line` type. The `TYPE` is an attribute of the source data
+     that can be seen by inspecting the GeoJSON source file, for example:
+
+    {
+      "type": "Feature",
+      "properties": {
+        "NAME": "Dupont Circle",
+        "TYPE": "metro-station"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.043416,
+          38.909605
+        ]
+      },
+      "id": "994446c244acadeb15d3f9fc18278c73"
+    }
+    */
     MGLLineStyleLayer *lineLayer = [[MGLLineStyleLayer alloc] initWithIdentifier:@"rail-line" source: source];
     lineLayer.predicate = [NSPredicate predicateWithFormat:@"TYPE = 'Rail line'"];
     lineLayer.lineColor = [NSExpression expressionForConstantValue:[UIColor redColor]];
     lineLayer.lineWidth = [NSExpression expressionForConstantValue:@2];
-    
-    // Add style layers to the map view's style.
-    [self.mapView.style addLayer:circleLayer];
-    [self.mapView.style insertLayer:lineLayer belowLayer:circleLayer];
+
+    [self addLayer:lineLayer];
+
+}
+
+- (void)addPointsFrom:(MGLShapeSource *)source {
+    // Configure a circle style layer to represent rail stations, filtering out all data from
+    // the source that is not of `metro-station` type.
+    MGLCircleStyleLayer *circleLayer = [[MGLCircleStyleLayer alloc] initWithIdentifier:@"stations" source:source];
+    circleLayer.predicate = [NSPredicate predicateWithFormat:@"TYPE = 'metro-station'"];
+    circleLayer.circleColor = [NSExpression expressionForConstantValue:[UIColor redColor]];
+    circleLayer.circleRadius = [NSExpression expressionForConstantValue:@6];
+    circleLayer.circleStrokeWidth = [NSExpression expressionForConstantValue:@2];
+    circleLayer.circleStrokeColor = [NSExpression expressionForConstantValue:[UIColor blackColor]];
+
+    [self addLayer:circleLayer];
+}
+
+- (void)addPolygonsFrom:(MGLShapeSource *)source {
+    // Configure a fill style layer to represent polygon regions in Washington, D.C.
+    // Source data that is not of `neighborhood-region` type will be excluded.
+    MGLFillStyleLayer *polygonLayer = [[MGLFillStyleLayer alloc] initWithIdentifier:@"DC-regions" source: source];
+    polygonLayer.predicate = [NSPredicate predicateWithFormat:@"TYPE = 'neighborhood-region'"];
+    polygonLayer.fillColor = [NSExpression expressionForConstantValue:[UIColor colorWithRed: 0.27 green: 0.41 blue: 0.97 alpha: 0.3]];
+    polygonLayer.fillOutlineColor = [NSExpression expressionForConstantValue:[UIColor colorWithRed: 0.27 green: 0.41 blue: 0.97 alpha: 1.0]];
+
+    [self addLayer:polygonLayer];
 }
 
 @end
