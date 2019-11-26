d0824559f16ebf1ab1d3b250692545d8d6b557ed
Nishant Karajgikar <nishant.karajgikar@mapbox.com>
2019-11-26 16:41:51 -0500

    Simplifying ornament layout example

--- Examples/ObjectiveC/OrnamentsLayoutExample.m
+++ Examples/ObjectiveC/OrnamentsLayoutExample.m
@@ -4,38 +4,8 @@
 
 // Ornament positions matrix arry, this example will demonstrate how ornament looks like in different positions.
 NSString *const MBXExampleOrnamentsLayout = @"OrnamentsLayoutExample";
-MGLOrnamentPosition ornamentPositions[4][4] = {
-    {
-        MGLOrnamentPositionTopLeft,
-        MGLOrnamentPositionTopRight,
-        MGLOrnamentPositionBottomRight,
-        MGLOrnamentPositionBottomLeft
-    },
-    {
-        MGLOrnamentPositionTopRight,
-        MGLOrnamentPositionBottomRight,
-        MGLOrnamentPositionBottomLeft,
-        MGLOrnamentPositionTopLeft
-    },
-    {
-        MGLOrnamentPositionBottomRight,
-        MGLOrnamentPositionBottomLeft,
-        MGLOrnamentPositionTopLeft,
-        MGLOrnamentPositionTopRight
-    },
-    {
-        MGLOrnamentPositionBottomLeft,
-        MGLOrnamentPositionTopLeft,
-        MGLOrnamentPositionTopRight,
-        MGLOrnamentPositionBottomRight
-    }
-};
 
 @interface OrnamentsLayoutExample ()<MGLMapViewDelegate>
-
-@property (nonatomic) MGLMapView *mapView;
-@property (nonatomic) NSTimer *timer;
-@property (nonatomic) NSInteger currentPositionIndex;
 
 @end
 
@@ -44,49 +14,28 @@ MGLOrnamentPosition ornamentPositions[4][4] = {
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    // Initialize mapView with some center
     MGLMapView *mapView = [[MGLMapView alloc] initWithFrame:self.view.bounds];
     mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     [mapView setCenterCoordinate:CLLocationCoordinate2DMake(39.915143, 116.404053)
                        zoomLevel:16
                        direction:30
                         animated:NO];
+    
+    // Configure mapView to show the scale and always display the compass
     mapView.delegate = self;
     mapView.showsScale = YES;
-    [self.view addSubview:mapView];
+    mapView.compassView.compassVisibility = MGLOrnamentVisibilityVisible;
     
-    self.mapView = mapView;
-}
-
-- (void)viewDidDisappear:(BOOL)animated {
-    [super viewDidDisappear:animated];
-    // Stop the timer if the view was removed.
-    [self.timer invalidate];
-    self.timer = nil;
-}
-
-- (void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-    // Create a timer to update ornaments position every second.
-    self.timer = [NSTimer scheduledTimerWithTimeInterval:1
-                                                  target:self
-                                                selector:@selector(onTimerTick)
-                                                userInfo:nil
-                                                 repeats:YES];
-}
-
-- (void)updateOrnamentsPosition {
-    // Get position matrix for current turn. We mod 4 to roll over the matrix array.
-    MGLOrnamentPosition *currentPosition = ornamentPositions[_currentPositionIndex % 4];
-    // Update ornaments position with position matrix.
-    self.mapView.scaleBarPosition = currentPosition[0];
-    self.mapView.compassViewPosition = currentPosition[1];
-    self.mapView.logoViewPosition = currentPosition[2];
-    self.mapView.attributionButtonPosition = currentPosition[3];
-}
-
-- (void)onTimerTick {
-    self.currentPositionIndex ++;
-    [self updateOrnamentsPosition];
+    // Set positions of various ornaments via `MGLOrnamentPosition` enum.
+    // NOTE: You can be more prescriptive about where the ornaments are positioned by using
+    // the `scaleBarMargins` , `compassViewMargins`, `logoViewMargins` and `attributionButtonMargins` properties.
+    mapView.scaleBarPosition = MGLOrnamentPositionTopLeft;
+    mapView.compassViewPosition = MGLOrnamentPositionTopRight;
+    mapView.logoViewPosition = MGLOrnamentPositionBottomLeft;
+    mapView.attributionButtonPosition = MGLOrnamentPositionBottomRight;
+    
+    [self.view addSubview:mapView];
 }
 
 @end
--- Examples/Swift/OrnamentsLayoutExample.swift
+++ Examples/Swift/OrnamentsLayoutExample.swift
@@ -1,59 +1,30 @@
 import Mapbox
 
-// Ornament positions matrix array, this example will demonstrate how ornament looks like in different positions.
-let ornamentPosition: [[MGLOrnamentPosition]] = [
-    [.topLeft, .topRight, .bottomRight, .bottomLeft],
-    [.topRight, .bottomRight, .bottomLeft, .topLeft],
-    [.bottomRight, .bottomLeft, .topLeft, .topRight],
-    [.bottomLeft, .topLeft, .topRight, .bottomRight]
-]
-
 @objc(OrnamentsLayoutExample_Swift)
 class OrnamentsLayoutExample_Swift: UIViewController, MGLMapViewDelegate {
-    var mapView: MGLMapView!
-    var timer: Timer?
-    var currentPositionIndex: Int = 0
+    private var mapView: MGLMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Initializing map view
         mapView = MGLMapView(frame: view.bounds)
-        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        mapView.showsScale = true
         mapView.setCenter(CLLocationCoordinate2DMake(39.915143, 116.404053), zoomLevel: 16, direction: 30, animated: false)
-        self.view.addSubview(mapView)
-
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        // Configure mapView to show the scale and always display the compass
         mapView.delegate = self
-    }
+        mapView.showsScale = true
+        mapView.compassView.compassVisibility = .visible
+        
+        // Set positions of various ornaments using the `MGLOrnamentPosition` enum.
+        // NOTE: You can be more prescriptive about where the ornaments are positioned by using
+        // the `scaleBarMargins` , `compassViewMargins`, `logoViewMargins` and `attributionButtonMargins` properties.
+        mapView.scaleBarPosition = .topLeft
+        mapView.compassViewPosition = .topRight
+        mapView.logoViewPosition = .bottomLeft
+        mapView.attributionButtonPosition = .bottomRight
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        // Create a timer to update ornaments position every second.
-        self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(onTimerTick), userInfo: nil, repeats: true)
-    }
-
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        guard let timer = self.timer else {
-            return
-        }
-        // Stop the timer if the view was removed.
-        timer.invalidate()
-        self.timer = nil
-    }
-
-    func updateOrnamentsPosition() {
-        // Get position matrix for current turn. We mod 4 to roll over the matrix array.
-        let positions = ornamentPosition[self.currentPositionIndex%4]
-        // Update ornaments position with position matrix.
-        self.mapView.scaleBarPosition = positions[0]
-        self.mapView.compassViewPosition = positions[1]
-        self.mapView.logoViewPosition = positions[2]
-        self.mapView.attributionButtonPosition = positions[3]
-    }
-
-    @objc func onTimerTick() {
-        self.currentPositionIndex += 1
-        self.updateOrnamentsPosition()
+        view.addSubview(mapView)
     }
 }
