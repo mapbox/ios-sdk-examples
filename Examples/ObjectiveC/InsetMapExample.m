85a5ebb3d380a022305154edf7b03f588c3d72b3
ZiZi <zmiles@gwu.edu>
2020-06-23 13:41:15 -0400

    removing podfile.lock and making other requested margin changes.

--- Examples/ObjectiveC/InsetMapExample.m
+++ Examples/ObjectiveC/InsetMapExample.m
@@ -15,22 +15,35 @@ NSString *const MBXExampleInsetMap = @"InsetMapExample";
 
     self.mapView = [[MGLMapView alloc] initWithFrame:self.view.bounds];
     self.mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    // Set the main map's center coordinate and zoom level.
-    [self.mapView setCenterCoordinate:CLLocationCoordinate2DMake(18.1096,-77.2975)
-                zoomLevel:9
-                 animated:NO];
+    /**
+     Set the delegate property of our map view
+     to self after instantiating it.
+    */
+    self.mapView.delegate = self;
+    // Set the main map view's center coordinate and zoom level.
+    [self.mapView setCenterCoordinate:CLLocationCoordinate2DMake(
+        18.1096,-77.2975) zoomLevel:9 animated:NO];
 
-    // Set inset mapview properties to create a smaller, non-interactive mapView that mimics the appearance of the main mapView.
+    /**
+     Set inset map view properties to create a smaller,
+     non-interactive map view that mimics the appearance of the main map view.
+     */
     self.miniMapView = [[MGLMapView alloc] initWithFrame:CGRectZero];
     self.miniMapView.allowsScrolling = NO;
     self.miniMapView.allowsTilting = NO;
     self.miniMapView.allowsZooming = NO;
     self.miniMapView.rotateEnabled = NO;
-
     
-    // we are only hiding the attribution within the second map view. Ordinarily, hiding the map view's attribution would be a violation of TOS, however, because the main map view still has its attribution, there is no issue here.
-    [self.miniMapView.logoView setHidden:YES];
-    [self.miniMapView.compassView setHidden: YES];
+    /**
+     Hiding the map view's attribution goes against our attribution requirements.
+     However, because the main map view still has attribution, hiding the
+     attribution for the mini map view in this case is acceptable.
+
+     For more information, please refer to our attribution guidelines:
+     https://docs.mapbox.com/help/how-mapbox-works/attribution/
+    */
+    self.miniMapView.logoView.hidden = YES;
+    self.miniMapView.compassView.hidden = YES;
     self.miniMapView.attributionButton.tintColor = UIColor.clearColor;
     self.miniMapView.layer.borderColor = UIColor.blackColor.CGColor;
     self.miniMapView.layer.borderWidth = 1;
@@ -38,36 +51,40 @@ NSString *const MBXExampleInsetMap = @"InsetMapExample";
     [self.miniMapView setCenterCoordinate:self.mapView.centerCoordinate zoomLevel:self.mapView.zoomLevel animated:NO];
     [self.miniMapView setTranslatesAutoresizingMaskIntoConstraints:NO];
 
-    // Set the delegate property of our map view to self after instantiating it
-    self.mapView.delegate = self;
-
     [self.view addSubview:self.mapView];
     [self.view addSubview:self.miniMapView];
     [self installConstraints];
 
-    
 }
 
+    /**
+     Set the mini map view's camera to the map view camera so while the region is changing on the
+     map view, the same camera changes are made in the mini map view.
+    */
 - (void)mapView:(MGLMapView *)mapView regionWillChangeAnimated:(BOOL)animated {
     [self.miniMapView setCamera:self.mapView.camera animated:NO];
 }
 
-
 - (void)installConstraints {
-
 
     if (@available(iOS 11, *)) {
         UILayoutGuide *safeArea = self.view.safeAreaLayoutGuide;
         NSArray *constraints = @[
-        [self.miniMapView.bottomAnchor constraintEqualToAnchor: safeArea.bottomAnchor constant:-40],
-        [self.miniMapView.trailingAnchor constraintEqualToAnchor: safeArea.trailingAnchor constant:-15],
-        [self.miniMapView.widthAnchor constraintEqualToAnchor:safeArea.widthAnchor multiplier:0.33],
-        [self.miniMapView.heightAnchor constraintEqualToAnchor:self.miniMapView.widthAnchor]
+        [self.miniMapView.bottomAnchor constraintEqualToAnchor:
+         safeArea.bottomAnchor constant:-40],
+        [self.miniMapView.trailingAnchor constraintEqualToAnchor:
+         safeArea.trailingAnchor constant:-15],
+        [self.miniMapView.widthAnchor constraintEqualToAnchor:
+         safeArea.widthAnchor multiplier:0.33],
+        [self.miniMapView.heightAnchor constraintEqualToAnchor:
+         self.miniMapView.widthAnchor]
         ];
     
         [NSLayoutConstraint activateConstraints:constraints];
     } else {
-        self.miniMapView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
+        self.miniMapView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin
+        | UIViewAutoresizingFlexibleLeftMargin |
+        UIViewAutoresizingFlexibleRightMargin;
     }
 }
 
--- Examples/Swift/InsetMapExample.swift
+++ Examples/Swift/InsetMapExample.swift
@@ -11,27 +11,44 @@ class InsetMapExample_Swift: UIViewController, MGLMapViewDelegate {
 
         mapView = MGLMapView(frame: view.bounds)
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        /**
+         Set the delegate property of our map view
+         to self after instantiating it.
+        */
         mapView.delegate = self
         
-        // Set the main map's center coordinate and zoom level.
-        mapView.setCenter(CLLocationCoordinate2D(latitude: 18.1096, longitude: -77.2975), zoomLevel: 9, animated: false)
-
-        // Set inset mapView's center
+        // Set the main map view's center coordinate and zoom level.
+        mapView.setCenter(CLLocationCoordinate2D(
+            latitude: 18.1096, longitude: -77.2975), zoomLevel: 9,
+                                                     animated: false)
+        
+        // Set inset map View's center
         miniMapview = MGLMapView(frame: CGRect.zero)
         
-        // Set inset mapview properties to create a smaller, non-interactive mapView that mimics the appearance of the main mapView.
+        /**
+         Set inset mapview properties to create a smaller,
+         non-interactive map view that mimics the appearance of the main map view.
+        */
         miniMapview.allowsScrolling = false
         miniMapview.allowsTilting = false
         miniMapview.allowsZooming = false
         miniMapview.allowsRotating = false
         miniMapview.compassView.isHidden = true
-        
-        // we are only hiding the attribution within the second map view. Ordinarily, hiding the map view's attribution would be a violation of TOS, however, because the main map view still has its attribution, there is no issue here.
+
+        /**
+         Hiding the map view's attribution goes against our attribution requirements.
+         However, because the main map view still has attribution, hiding the
+         attribution for the mini map view in this case is acceptable.
+
+         For more information, please refer to our attribution guidelines:
+         https://docs.mapbox.com/help/how-mapbox-works/attribution/
+        */
         miniMapview.logoView.isHidden = true
         miniMapview.attributionButton.tintColor = UIColor.clear
         miniMapview.layer.borderColor = UIColor.black.cgColor
         miniMapview.layer.borderWidth = 1
-        miniMapview.setCenter(self.mapView.centerCoordinate, zoomLevel: mapView.zoomLevel, animated: false)
+        miniMapview.setCenter(self.mapView.centerCoordinate,
+                              zoomLevel: mapView.zoomLevel, animated: false)
         miniMapview.translatesAutoresizingMaskIntoConstraints = false
         
         view.addSubview(mapView)
@@ -40,7 +57,10 @@ class InsetMapExample_Swift: UIViewController, MGLMapViewDelegate {
         installConstraints()
     }
     
-    // Set the miniMapView's camera to the mapView camera so that while the region is changing on the mapView, the same camera changes are made in the miniMapview.
+    /**
+     Set the mini map view's camera to the map view camera so while the region is changing on the
+     map view, the same camera changes are made in the mini map view.
+     */
     func mapViewRegionIsChanging(_ mapView: MGLMapView) {
         miniMapview.setCamera(mapView.camera, animated: false)
     }
@@ -49,13 +69,19 @@ class InsetMapExample_Swift: UIViewController, MGLMapViewDelegate {
         if #available(iOS 11.0, *) {
             let safeArea = self.view.safeAreaLayoutGuide
             NSLayoutConstraint.activate([
-                miniMapview.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -40),
-                miniMapview.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -15),
-                miniMapview.widthAnchor.constraint(equalTo: safeArea.widthAnchor, multiplier: 0.33),
-                miniMapview.heightAnchor.constraint(equalTo: miniMapview.widthAnchor)
+                miniMapview.bottomAnchor.constraint(equalTo:
+                    safeArea.bottomAnchor, constant: -40),
+                miniMapview.trailingAnchor.constraint(equalTo:
+                    safeArea.trailingAnchor, constant: -15),
+                miniMapview.widthAnchor.constraint(equalTo:
+                    safeArea.widthAnchor, multiplier: 0.33),
+                miniMapview.heightAnchor.constraint(equalTo:
+                    miniMapview.widthAnchor)
             ])
         } else {
-            miniMapview.autoresizingMask = [.flexibleTopMargin, .flexibleLeftMargin, .flexibleRightMargin]
+            miniMapview.autoresizingMask = [.flexibleTopMargin,
+                                            .flexibleLeftMargin,
+                                            .flexibleRightMargin]
         }
     }
 }
