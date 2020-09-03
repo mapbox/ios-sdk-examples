6d4ece59e82207e55b9f93721ef4bc1e8a3bd5e4
Takuto Suzuki <takuto.suzuki@mapbox.com>
2020-09-03 16:45:33 +0900

    testing 6.2.0-alpha1 with user location

--- Examples/Swift/SimpleMapViewExample.swift
+++ Examples/Swift/SimpleMapViewExample.swift
@@ -2,15 +2,39 @@ import Mapbox
 
 @objc(SimpleMapViewExample_Swift)
 
-class SimpleMapViewExample_Swift: UIViewController {
+class SimpleMapViewExample_Swift: UIViewController, MGLMapViewDelegate, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         let mapView = MGLMapView(frame: view.bounds)
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-
-        // Set the map’s center coordinate and zoom level.
-        mapView.setCenter(CLLocationCoordinate2D(latitude: 59.31, longitude: 18.06), zoomLevel: 9, animated: false)
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        mapView.setCenter(CLLocationCoordinate2D(latitude: 35.66903, longitude: 139.759947), zoomLevel: 17, animated: false)
         view.addSubview(mapView)
+    }
+
+    func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
+        
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.distanceFilter=kCLDistanceFilterNone
+            locationManager.startUpdatingLocation()
+        }
+        
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
     }
 }
