e02b6ec9a4793a32da2cc052bc3ce81b3b2a1779
Jordan Kiley <jmkiley@users.noreply.github.com>
2020-09-24 09:01:38 -0700

    Update for 6.2.1 (#409)
    * Update for 6.2.0-alpha.1
    
    * Update to 6.2.0-beta.2 (#412)
    
    * iOS 14 privacy example (#411)
    
    * Update Gemfile.lock (#410)
    
    * privacy example with updated pod pointing to latest maps sdk
    
    * cleaned up comments
    
    * updating descriptions in plist, reformatted comments, and doing swift example the correct way
    
    * updating example to reflect the correct use case of what should be happening
    
    * adding ios 14 check
    
    * address pr comments
    
    * should be using the actualy key that is in the plist
    
    * disabling test for weird failures
    
    Co-authored-by: Jordan Kiley <jmkiley@users.noreply.github.com>
    
    * Bump sdk version to 6.2.1
    
    Co-authored-by: Fabian Guerra Soto <fabian.guerra@mapbox.com>
    Co-authored-by: Neel Mistry <Neel.mistry@mapbox.com>

---
+++ Examples/ObjectiveC/LocationPrivacyExample.m
@@ -0,0 +1,77 @@
+#import "LocationPrivacyExample.h"
+@import Mapbox;
+
+NSString *const MBXExampleLocationPrivacy = @"LocationPrivacyExample";
+
+@interface LocationPrivacyExample () <MGLMapViewDelegate>
+@property (nonatomic) MGLMapView *mapView;
+@property (nonatomic) UIButton *preciseButton;
+@end
+
+@implementation LocationPrivacyExample
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    MGLMapView *mapView = [[MGLMapView alloc] initWithFrame:self.view.bounds];
+    mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    mapView.delegate = self;
+    mapView.showsUserLocation = YES;
+    self.mapView = mapView;
+
+    [self.view addSubview:mapView];
+}
+
+/**
+    In order to enable the alert that requests temporary precise location,
+    please add the following key to your info.plist
+    @c NSLocationTemporaryUsageDescriptionDictionary
+
+    You must then add
+    @c MGLAccuracyAuthorizationDescription
+    as a key in the Privacy - Location Temporary Usage Description Dictionary
+ */
+- (void)mapView:(MGLMapView *)mapView didChangeLocationManagerAuthorization:(id<MGLLocationManager>)manager {
+    if (@available(iOS 14, *)) {
+        if (manager.accuracyAuthorization == CLAccuracyAuthorizationReducedAccuracy) {
+            [self addPreciseButton];
+        } else {
+            [self removePreciseButton];
+        }
+    }
+}
+
+- (void)addPreciseButton {
+    UIButton *preciseButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [preciseButton setTitle:@"Turn Precise On" forState:UIControlStateNormal];
+    preciseButton.backgroundColor = UIColor.grayColor;
+
+    [preciseButton addTarget:self action:@selector(requestTemporaryAuth) forControlEvents:UIControlEventTouchDown];
+    self.preciseButton = preciseButton;
+    [self.view addSubview:preciseButton];
+
+    // constraints
+    [preciseButton setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [preciseButton.widthAnchor constraintEqualToConstant:150.0].active = YES;
+    [preciseButton.heightAnchor constraintEqualToConstant:40.0].active = YES;
+    [preciseButton.topAnchor constraintEqualToAnchor:self.view.topAnchor constant:100.0].active = YES;
+    [preciseButton.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor].active = YES;
+}
+
+- (void)requestTemporaryAuth {
+    if (self.mapView != nil) {
+        NSString *purposeKey = @"MGLAccuracyAuthorizationDescription";
+        if (@available(iOS 14, *)) {
+            [self.mapView.locationManager requestTemporaryFullAccuracyAuthorizationWithPurposeKey:purposeKey];
+        }
+    }
+}
+
+- (void)removePreciseButton {
+    if (self.preciseButton != nil) {
+        [self.preciseButton removeFromSuperview];
+        self.preciseButton = nil;
+    }
+}
+
+@end
--- Podfile
+++ Podfile
@@ -2,7 +2,7 @@ platform :ios, '9.0'
 use_frameworks!
 
 def shared_pods
-    pod 'Mapbox-iOS-SDK', '6.1.0'
+    pod 'Mapbox-iOS-SDK', '6.2.1'
     pod 'SwiftLint', '~> 0.29'
 end
 
