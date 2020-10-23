1c24c5fb176d64a7dce3a15671959fc99036d622
ZiZi <zmiles@gwu.edu>
2020-10-23 16:01:40 -0400

    added changes

--- Examples/Swift/ManageOfflineRegionsExample.swift
+++ Examples/Swift/ManageOfflineRegionsExample.swift
@@ -3,9 +3,8 @@ import Foundation
 
 @objc(ManageOfflineRegionsExample_Swift)
 
+class ManageOfflineRegionsExample_Swift: UIViewController, MGLMapViewDelegate, UITableViewDelegate, UITableViewDataSource {
 
-class ManageOfflineRegionsExample_Swift: UIViewController, MGLMapViewDelegate, UITableViewDelegate, UITableViewDataSource  {
-    
     lazy var mapView: MGLMapView = {
         let mapView = MGLMapView(frame: CGRect.zero)
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
@@ -17,7 +16,7 @@ class ManageOfflineRegionsExample_Swift: UIViewController, MGLMapViewDelegate, U
     lazy var downloadButton: UIButton = {
         let downloadButton = UIButton(frame: CGRect.zero)
         downloadButton.backgroundColor = UIColor.white
-        downloadButton.setTitleColor(.black, for: .normal)
+        downloadButton.setTitleColor(UIColor.systemBlue, for: .normal)
         downloadButton.setTitle("Download Region", for: .normal)
         downloadButton.addTarget(self, action: #selector(startOfflinePackDownload), for: .touchUpInside)
         downloadButton.layer.cornerRadius = view.bounds.width / 30
@@ -32,7 +31,7 @@ class ManageOfflineRegionsExample_Swift: UIViewController, MGLMapViewDelegate, U
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     }()
-    var progressPercentage: Float?
+    var cell = UITableViewCell()
     // make an extension for this and make necessary comments and push
     override func viewDidLoad() {
 
@@ -47,59 +46,43 @@ class ManageOfflineRegionsExample_Swift: UIViewController, MGLMapViewDelegate, U
 
         // Setup offline pack notification handlers.
         addObserver()
+        installConstraints()
 
-        if #available(iOS 11.0, *) {
-            installConstraints()
-        } else {
-            // Fallback on earlier versions
-        }
     }
 
     func addObserver() {
         NotificationCenter.default.addObserver(self, selector: #selector(offlinePackProgressDidChange), name: NSNotification.Name.MGLOfflinePackProgressChanged, object: nil)
     }
 
-    @available(iOS 11.0, *)
     func installConstraints() {
 
         NSLayoutConstraint.activate([
-            mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            mapView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            mapView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            mapView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.75),
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mapView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.75),
             tableView.topAnchor.constraint(equalTo: mapView.bottomAnchor),
             tableView.leadingAnchor.constraint(equalTo: mapView.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: mapView.trailingAnchor),
             tableView.heightAnchor.constraint(equalTo: mapView.heightAnchor, multiplier: 0.4),
-            downloadButton.topAnchor.constraint(equalTo: mapView.topAnchor, constant: 10),
-            downloadButton.leadingAnchor.constraint(equalTo: mapView.leadingAnchor),
+            downloadButton.topAnchor.constraint(equalTo: mapView.topAnchor, constant: 100),
+            downloadButton.leadingAnchor.constraint(equalTo: mapView.leadingAnchor, constant: 5),
             downloadButton.trailingAnchor.constraint(equalTo: mapView.leadingAnchor, constant: 150)
         ])
 
     }
 
     override func viewDidDisappear(_ animated: Bool) {
-
         super.viewDidDisappear(animated)
-        // When leaving this view controller, remove all offline downloads.
-        guard let packs = MGLOfflineStorage.shared.packs else { return }// or do something else }
-        MGLOfflineStorage.shared.removePack(packs[0]) { (error) in
-            // handle error if pack can't be removed
+
+        MGLOfflineStorage.shared.resetDatabase { (error) in
             if let error = error {
                 print(error)
             } else {
+                MGLOfflineStorage.shared.reloadPacks()
                 self.tableView.reloadData()
             }
-            //mechanisms to reload certain cells instead of entire tableview
         }
-
-    }
-
-    deinit {
-        // Remove offline pack observers.
-        print("Removing offline pack notification observers")
-        NotificationCenter.default.removeObserver(self)
-
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -111,22 +94,27 @@ class ManageOfflineRegionsExample_Swift: UIViewController, MGLMapViewDelegate, U
         }
     }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
-        if let packs = MGLOfflineStorage.shared.packs {
-            let completed = Float(packs[indexPath.row].progress.countOfResourcesCompleted)
-                let expected = Float(packs[indexPath.row].progress.countOfResourcesExpected)
-                let progress = completed / expected
-            cell.textLabel?.text = "Region \(indexPath.row + 1): size: \(packs[indexPath.row].progress.countOfBytesCompleted)"
-            cell.detailTextLabel?.text = "Percent completion: \(String(format: "%.2f", progress * 100))%"
-            cell.textLabel?.textAlignment = .left
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let label = UILabel()
+        if section == 0 {
+            label.backgroundColor = UIColor.white
+            label.textAlignment = .center
+            label.text = "No Offline Packs Saved"
+            return label.text
+        }
 
-        } else {
-            print("so...")
-            cell.textLabel?.text = "No Offline Packs Saved"
-            cell.textLabel?.textColor = .darkGray
-            cell.textLabel?.textAlignment = .center
+        return nil
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+
+        if let packs = MGLOfflineStorage.shared.packs {
+
+            cell.textLabel?.text = "Region \(indexPath.row + 1): size: \(packs[indexPath.row].progress.countOfBytesCompleted)"
+            cell.detailTextLabel?.text = "Percent completion: \(String(format: "%.2f", packs[indexPath.row].progress.progressPercentage))%"
+            cell.textLabel?.textAlignment = .left
 
         }
 
@@ -138,32 +126,6 @@ class ManageOfflineRegionsExample_Swift: UIViewController, MGLMapViewDelegate, U
         guard let packs = MGLOfflineStorage.shared.packs else { return }
         if let selectedRegion = packs[indexPath.row].region as? MGLTilePyramidOfflineRegion {
             mapView.setVisibleCoordinateBounds(selectedRegion.bounds, animated: true)
-        }
-    }
-
-    @available(iOS 11.0, *)
-    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        return UISwipeActionsConfiguration(actions: [
-            makeDeleteContextualAction(forRowAt: indexPath)
-        ])
-    }
-
-    @available(iOS 11.0, *)
-    private func makeDeleteContextualAction(forRowAt indexPath: IndexPath) -> UIContextualAction {
-        return UIContextualAction(style: .destructive, title: "Delete") { (action, swipeButtonView, completion) in
-            guard let packs = MGLOfflineStorage.shared.packs else { return }
-            MGLOfflineStorage.shared.removePack(packs[indexPath.row]) { (error) in
-                // handle error if pack can't be removed
-                if let error = error {
-                    print(error)
-                } else {
-                    self.tableView.reloadData()
-                }
-
-                //mechanisms to reload certain cells instead of entire tableview
-            }
-
-            completion(true)
         }
     }
 
@@ -180,7 +142,7 @@ class ManageOfflineRegionsExample_Swift: UIViewController, MGLMapViewDelegate, U
 
         // Create and register an offline pack with the shared offline storage object.
 
-        MGLOfflineStorage.shared.addPack(for: region, withContext: context) { [self] (pack, error) in
+        MGLOfflineStorage.shared.addPack(for: region, withContext: context) { (pack, error) in
             guard error == nil else {
                 // The pack couldn’t be created for some reason.
                 print("Error: \(error?.localizedDescription ?? "unknown error")")
@@ -202,14 +164,9 @@ class ManageOfflineRegionsExample_Swift: UIViewController, MGLMapViewDelegate, U
         // and the associated user info for the pack; in this case, `name = My Offline Pack`
         if let pack = notification.object as? MGLOfflinePack,
            let userInfo = NSKeyedUnarchiver.unarchiveObject(with: pack.context) as? [String: String] {
-            let progress = pack.progress
-            // or notification.userInfo![MGLOfflinePackProgressUserInfoKey]!.MGLOfflinePackProgressValue
-            let completedResources = progress.countOfResourcesCompleted
-            let expectedResources = progress.countOfResourcesExpected
 
-            // Calculate current progress percentage.
-
-            progressPercentage = Float(completedResources) / Float(expectedResources)
+            let completedResources = pack.progress.countOfResourcesCompleted
+            let expectedResources = pack.progress.countOfResourcesExpected
 
             if completedResources == expectedResources {
 
@@ -221,14 +178,30 @@ class ManageOfflineRegionsExample_Swift: UIViewController, MGLMapViewDelegate, U
 
                 // Otherwise, print download/verification progress.
 
-                print("Offline pack “\(userInfo["name"] ?? "unknown")” has \(completedResources) of \(expectedResources) resources — \(String(format: "%.2f", progressPercentage! * 100))%.")
+                print("Offline pack “\(userInfo["name"] ?? "unknown")” has \(completedResources) of \(expectedResources) resources — \(String(format: "%.2f", pack.progress.progressPercentage))%.")
 
             }
-
-            self.tableView.reloadData()
-
         }
+        self.tableView.reloadData()
 
     }
+
+}
+
+fileprivate extension MGLOfflinePackProgress {
+
+    var progressPercentage: Float {
+        if countOfResourcesExpected != 0 {
+            return Float((countOfResourcesCompleted / countOfResourcesExpected) * 100)
+        } else {
+            return 1.0
+        }
+    }
+//    var completedResources: Float {
+//        return Float(countOfResourcesCompleted)
+//    }
+//    var expectedResources: Float {
+//        return Float(countOfResourcesExpected)
+//    }
 
 }
