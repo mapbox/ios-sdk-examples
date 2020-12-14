4eefa4974a067de84c88ebf59f8cb67d15786a8a
ZiZi <44972592+ZiZasaurus@users.noreply.github.com>
2020-12-14 09:23:52 -0500

    Update Examples/ObjectiveC/ManageOfflineRegionsExample.m
    Co-authored-by: Nadia Barbosa <captainbarbosa@users.noreply.github.com>

--- Examples/ObjectiveC/ManageOfflineRegionsExample.m
+++ Examples/ObjectiveC/ManageOfflineRegionsExample.m
@@ -240,7 +240,7 @@ heightForHeaderInSection:(NSInteger)section {
     
     if (MGLOfflineStorage.sharedOfflineStorage.packs.count != 0 )  {
         MGLOfflinePack * pack = MGLOfflineStorage.sharedOfflineStorage.packs[indexPath.row];
-        long number = indexPath.row + 1;
+        NSInteger number = indexPath.row + 1;
         long countOfBytesCompleted = MGLOfflineStorage.sharedOfflineStorage.packs[indexPath.row].progress.countOfBytesCompleted;
         NSByteCountFormatter *formattedCountOfBytedCompleted = [NSByteCountFormatter stringFromByteCount:countOfBytesCompleted countStyle:NSByteCountFormatterCountStyleMemory];
         float countOfResourcesCompleted = pack.progress.countOfResourcesCompleted;
