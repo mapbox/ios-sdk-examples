ac041ee1b812d3a5eb8c46e28d349b67939235c9
Zizasaurus <zmiles@gwu.edu>
2021-01-19 19:45:45 -0500

    Revert "made requested changes"
    This reverts commit e1cde8c28217977db2ecf0b1bff5a71ae7c5c631.

--- Examples/ObjectiveC/Headers/LineGradientExample.h
+++ Examples/ObjectiveC/Headers/LineGradientExample.h
@@ -1,3 +1,11 @@
+//
+//  GradientGeoJSONLineExample.h
+//  Examples
+//
+//  Created by ZiZi Miles on 10/14/19.
+//  Copyright © 2019 Mapbox. All rights reserved.
+//
+
 #import <UIKit/UIKit.h>
 
 @interface LineGradientExample : UIViewController
